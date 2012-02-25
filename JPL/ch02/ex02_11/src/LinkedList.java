b7c60c04bcc0d0ed368d62c3e063c8466cd7c07f
Ato ARAKI <ato.araki@gmail.com>
2012-02-25 19:00:56 +0900

    Exercise 2.11: Add a toString method to LinkedList.

--- JPL/ch02/ex02_11/src/LinkedList.java
+++ JPL/ch02/ex02_11/src/LinkedList.java
@@ -7,16 +7,23 @@ public class LinkedList {
 		this.obj = obj;
 	}
 	
+	@Override
+	public String toString() {
+		String desc = this.obj.toString();
+		if (this.next != null) {
+			desc += "\n   | " + this.next.toString();
+		}
+		return desc;
+	}
+	
 	public static void main(String[] args) {
 		LinkedList l;
 		
 		l = new LinkedList(new Vehicle(12, 90, "1号"));
 		l.next = new LinkedList(new Vehicle(22, 180, "2号"));
 		l.next.next = new LinkedList(new Vehicle(28, 210, "3号"));
-
-		((Vehicle)l.obj).show();
-		((Vehicle)l.next.obj).show();
-		((Vehicle)l.next.next.obj).show();
+		
+		System.out.println(l);
 	}
 }
 
