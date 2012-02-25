b7136738bff2379028fa4ae3a43d23cf103b9e19
Ato ARAKI <ato.araki@gmail.com>
2012-02-25 18:16:26 +0900

    add ex02_11

---
+++ JPL/ch02/ex02_11/src/LinkedList.java
@@ -0,0 +1,22 @@
+
+public class LinkedList {
+	public Object obj;
+	public LinkedList next;
+	
+	public LinkedList(Object obj) {
+		this.obj = obj;
+	}
+	
+	public static void main(String[] args) {
+		LinkedList l;
+		
+		l = new LinkedList(new Vehicle(12, 90, "1号"));
+		l.next = new LinkedList(new Vehicle(22, 180, "2号"));
+		l.next.next = new LinkedList(new Vehicle(28, 210, "3号"));
+
+		((Vehicle)l.obj).show();
+		((Vehicle)l.next.obj).show();
+		((Vehicle)l.next.next.obj).show();
+	}
+}
+
