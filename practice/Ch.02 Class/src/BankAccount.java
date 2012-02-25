c695dbc070977df0dfe5681563fa81c2a81e10af
Ato ARAKI <ato.araki@gmail.com>
2012-02-25 22:15:17 +0900

    I read P.53

---
+++ practice/Ch.02 Class/src/BankAccount.java
@@ -0,0 +1,40 @@
+
+public class BankAccount {
+	public class Person {
+
+	}
+
+	@SuppressWarnings("unused")
+	private long number; //!< 口座番号
+	@SuppressWarnings("unused")
+	private long balance; //!< 現在の残高（単位はセント）
+	
+	/**
+	 * 複数の値を返すために、各フィールドを設定するメソッド P.53
+	 * @param who
+	 * @return
+	 */
+	public Permissions permissionsFor (Person who) {
+		Permissions perm = new Permissions();
+		perm.canDeposit = canDeposit(who);
+		perm.canWithdraw = canWithdraw(who);
+		perm.canClose = canClose(who);
+		return perm;
+	}
+
+	private boolean canClose(Person who) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	private boolean canWithdraw(Person who) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	private boolean canDeposit(Person who) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
---
+++ practice/Ch.02 Class/src/Permissions.java
@@ -0,0 +1,11 @@
+
+/**
+ * 特定の行為が許可されているかを表すブール値をおじするオブジェクトのクラス P.53
+ * @author ato
+ *
+ */
+public class Permissions {
+	public boolean canDeposit,
+				   canWithdraw,
+				   canClose;
+}
