79d4ee5fe2c31730f1ef9b3c78855308b247c8ea
Ato ARAKI <ato.araki@gmail.com>
2012-02-25 11:04:56 +0900

    read P.49 add method

---
+++ practice/Ch.02 Class/src/BodyPrint.java
@@ -0,0 +1,15 @@
+
+class BodyPrint {
+	
+	// mainメソッド P.49
+	public static void main(String[] args) { // method header
+		// method body
+		
+		Body sun = new Body("Sol", null);
+		Body earth = new Body("Earth", sun);
+		System.out.println("Body " + earth.name + 
+						   " orbits " + earth.orbits.name +
+						   " and has ID " + earth.idNum);
+	}
+
+}
