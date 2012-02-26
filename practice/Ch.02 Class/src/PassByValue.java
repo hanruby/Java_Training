cdfe00fb8380d7fc52410cd5437399ecf7e47c48
Ato ARAKI <ato.araki@gmail.com>
2012-02-26 17:50:23 +0900

    I read P.54

---
+++ practice/Ch.02 Class/src/PassByValue.java
@@ -0,0 +1,19 @@
+/**
+ * 値渡し P.54
+ * @author ato
+ *
+ */
+class PassByValue {
+    public static void main(String[] args) {
+        double one = 1.0;
+
+        System.out.println("before: one = " + one);
+        halveIt(one);
+        System.out.println("after:  one = " + one);
+    }
+
+    public static void halveIt(double arg) {
+        arg /= 2.0;     // arg を 2 で割る
+        System.out.println("halved: arg = " + arg);
+    }
+}
