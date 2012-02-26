e47b0501c25dcf95b8da25ccb7eb7f572afdbfbb
Ato ARAKI <ato.araki@gmail.com>
2012-02-26 18:01:40 +0900

    I read P.56

---
+++ practice/Ch.02 Class/src/PassRef.java
@@ -0,0 +1,22 @@
+/**
+ * 参照渡し P.55
+ * パラメータがオブジェクト参照の場合は、オブジェクトではなく、オブジェクト参照が値渡しされる。 
+ * オブジェクトが参照渡しされるのではない。
+ * @author ato
+ *
+ */
+class PassRef {
+    public static void main(String[] args) {
+        Body sirius = new Body("Sirius", null);
+
+        System.out.println("before: " + sirius);
+        commonName(sirius);
+        System.out.println("after:  " + sirius);
+    }
+
+    public static void commonName(Body bodyRef) {
+//    public static void commonName(final Body bodyRef) { // final宣言すると、nullにするところでコンパイルエラーになる P.56
+        bodyRef.name = "Dog Star";
+        bodyRef = null;
+    }
+}
