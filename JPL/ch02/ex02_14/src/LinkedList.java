1d7e3f87f1b0d8dacdfd521fb314cccac4d8e5d6
Ato ARAKI <ato.araki@gmail.com>
2012-02-26 18:43:28 +0900

    add ex02_14

---
+++ JPL/ch02/ex02_14/src/LinkedList.java
@@ -0,0 +1,43 @@
+
+public class LinkedList {
+	public Object obj;
+	public LinkedList next;
+	
+	public LinkedList(Object obj) {
+		this.obj = obj;
+	}
+	
+	@Override
+	public String toString() {
+		String desc = this.obj.toString();
+		if (this.next != null) {
+			desc += "\n   | " + this.next.toString();
+		}
+		return desc;
+	}
+	
+	/**
+	 * ex02_12 
+	 * 可変長の引数で一括してobjectを登録するメソッドを書いてみた。
+	 * comment: 一括で登録できるため便利だと思う。
+	 * @param objs
+	 */
+	public void setObjects(Object... objs) {
+		LinkedList l = this;
+		for (Object object : objs) {
+			l.next = new LinkedList(object);
+			l = l.next; 
+		}
+	}
+	
+	public static void main(String[] args) {
+		LinkedList l;
+		
+		l = new LinkedList(new Vehicle(12, 90, "1号"));
+		l.next = new LinkedList(new Vehicle(22, 180, "2号"));
+		l.next.next = new LinkedList(new Vehicle(28, 210, "3号"));
+		
+		System.out.println(l);
+	}
+}
+
---
+++ JPL/ch02/ex02_14/src/LinkedListTest.java
@@ -0,0 +1,42 @@
+import static org.junit.Assert.*;
+//import junit.framework.Assert;
+
+import org.junit.Test;
+
+
+public class LinkedListTest {
+
+	@Test
+	public void testLinkedList() {
+		LinkedList l = new LinkedList("list1");
+		
+		assertEquals("list1", l.obj);
+		assertEquals(null, l.next);
+	}
+	
+	@Test
+	public void testLinkedList2() throws Exception {
+		LinkedList l = new LinkedList("list1");
+		l.next = new LinkedList("list2");
+		
+		assertEquals("list1", l.obj);
+		assertEquals("list2", l.next.obj);
+		assertEquals(null, l.next.next);
+	}
+	
+	@Test
+	public void testSetObjects() throws Exception {
+		LinkedList l = new LinkedList(new Vehicle(12, 90, "1号"));
+		
+		Vehicle v2 = new Vehicle(22, 180, "2号");
+		Vehicle v3 = new Vehicle(28, 210, "3号");
+		
+		l.setObjects(v2,v3);
+		
+		assertEquals(((Vehicle)(l.obj)).getOwnerName(), "1号");
+		assertEquals(((Vehicle)(l.next.obj)).getOwnerName(), "2号");
+		assertEquals(((Vehicle)(l.next.next.obj)).getOwnerName(), "3号");
+		
+		System.out.println(l);
+	}
+}
