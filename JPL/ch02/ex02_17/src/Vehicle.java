69130d28320f060ca1999911d414c3ad2dd7b093
Ato ARAKI <ato.araki@gmail.com>
2012-03-03 09:57:15 +0900

    add ex02_17

---
+++ JPL/ch02/ex02_17/src/Vehicle.java
@@ -0,0 +1,126 @@
+
+public class Vehicle {
+	private int currentSpeed;
+	private int currentDirection;
+	private String ownerName;
+	private long id;
+	
+	static private long nextVehicleId = 0;
+
+	//! IDを取得するメソッド
+	public long getId() {
+		return id;
+	}
+
+	//! speedを取得するメソッド
+	public int getCurrentSpeed() {
+		return currentSpeed;
+	}
+
+	//! speedを設定するメソッド
+	public void setCurrentSpeed(int currentSpeed) {
+		this.currentSpeed = currentSpeed;
+	}
+
+	//! directionを取得するメソッド
+	public int getCurrentDirection() {
+		return currentDirection;
+	}
+
+	//! directionを設定するメソッド
+	public void setCurrentDirection(int currentDirection) {
+		this.currentDirection = currentDirection;
+	}
+
+	//! nameを取得するメソッド
+	public String getOwnerName() {
+		return ownerName;
+	}
+
+	//! nameを設定するメソッド
+	public void setOwnerName(String ownerName) {
+		this.ownerName = ownerName;
+	}
+
+	/**
+	 * 初期化ブロック(P47)
+	 */
+	{
+		this.setCurrentSpeed(0);
+		this.setCurrentDirection(0);
+		nextVehicleId++;
+		this.id = nextVehicleId;
+	}
+	
+	/**
+	 * 引数なしコンストラクタ
+	 */
+	public Vehicle() {
+		this.setOwnerName("none");
+	}
+	
+	/**
+	 * 所有者の名前を引数にとるコンストラクタ
+	 */
+	public Vehicle(String name) {
+		this.setOwnerName(name);
+	}
+
+	public Vehicle(int speed, int direction, String name) {
+		this.setValue(speed, direction, name);
+	}
+	
+	public void setValue(int speed, int direction, String name) {
+		this.setCurrentSpeed(speed);
+		this.setCurrentDirection(direction);
+		this.setOwnerName(name);
+	}
+
+	public void setValue(int speed, int direction) {
+		this.setCurrentSpeed(speed);
+		this.setCurrentDirection(direction);
+	}
+	
+	/**
+	 * 識別番号の最大値を返すメソッド ex2.9
+	 * @return
+	 */
+	public static long getMaxId() {
+		return nextVehicleId;
+	}
+	
+	/**
+	 * toString ex2.10
+	 */
+	public String toString() {
+		return "  name:" + this.ownerName + 
+					  "  speed:" + this.currentSpeed +
+					  "  direction:" + this.currentDirection +
+					  "  id:" + this.id;
+	}
+	
+	/**
+	 * ex2.15 乗り物の現在のスピードを引数で渡された値に変更する
+	 */
+	public void changeSpeed(int speed) {
+		this.currentSpeed = speed;
+		System.out.println("changed speed : " + speed);
+	}
+	
+	/**
+	 * ex2.15 スピードをゼロにする
+	 */
+	public void stop() {
+		this.currentSpeed = 0;
+		System.out.println("stopped");
+	}
+	
+	public static void main(String[] args) {
+		Vehicle v1 = new Vehicle("new car"); 
+		Vehicle v2 = new Vehicle(); 
+		v1.setValue(100, 23);
+		v2.setValue(213, 34);
+		System.out.println(v1);
+		System.out.println(v2);
+ 	}
+}
---
+++ JPL/ch02/ex02_17/src/VehicleTest.java
@@ -0,0 +1,64 @@
+import static org.junit.Assert.*;
+//import junit.framework.Assert;
+
+import org.junit.Test;
+
+
+public class VehicleTest {
+
+	@Test
+	public void testVehicleInit() {
+		Vehicle v = new Vehicle();
+
+		assertEquals(0, v.getCurrentSpeed());
+		assertEquals(0, v.getCurrentDirection());
+		assertEquals("none", v.getOwnerName());
+		assertEquals(1, v.getId());
+	}
+	
+	@Test
+	public void testVehicle() {
+		Vehicle v = new Vehicle();
+		v.setValue(100, 90, "hogehoge");
+
+		assertEquals(100, v.getCurrentSpeed());
+		assertEquals(90, v.getCurrentDirection());
+		assertEquals("hogehoge", v.getOwnerName());
+		assertEquals(2, v.getId());
+	}
+	
+	@Test
+	public void testVehicle2() throws Exception {
+		Vehicle v = new Vehicle("hogehoge");
+		v.setValue(100, 90);
+
+		assertEquals(100, v.getCurrentSpeed());
+		assertEquals(90, v.getCurrentDirection());
+		assertEquals("hogehoge", v.getOwnerName());
+		assertEquals(3, v.getId());
+	}
+	
+	@Test
+	public void testGetId() throws Exception {
+		long max = Vehicle.getMaxId();
+		assertEquals(3, max);
+	}
+	
+	@Test
+	public void testChangeSpeed() throws Exception {
+		Vehicle v = new Vehicle("hogehoge");
+		v.setCurrentSpeed(150);
+		v.changeSpeed(120);
+		
+		assertEquals(120, v.getCurrentSpeed());
+	}
+	
+	@Test
+	public void tesetStop() throws Exception {
+		Vehicle v = new Vehicle("hogehoge");
+		v.setCurrentSpeed(150);
+		v.stop();
+		
+		assertEquals(0, v.getCurrentSpeed());
+	}
+}
