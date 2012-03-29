992b89fede6ff1907f81960ee522d6703739a92a
Ato ARAKI <ato.araki@gmail.com>
2012-03-29 22:18:53 +0900

    add walkman

---
+++ JPL/ch01/ex01_14/src/WalkmanPair.java
@@ -0,0 +1,24 @@
+/**
+ * ふたりで同じテープが聞けるウォークマン（２つ端子を備えてる）
+ * @author ato
+ *
+ */
+public class WalkmanPair extends WalkmanBase{
+
+	public void out1() {
+		super.output();
+	}
+	
+	public void out2() {
+		super.output();		
+	}
+	
+	public static void main(String[] args) {
+		WalkmanPair wm = new WalkmanPair();
+		wm.setMusic("♪♬♫");
+		wm.play();
+		wm.out1();
+		wm.out2();
+		wm.stop();
+	}
+}
