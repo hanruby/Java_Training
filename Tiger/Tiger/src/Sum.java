72c9b2cd3611984a6f16b4a5965abb8207f9f9b5
Ato ARAKI <ato.araki@gmail.com>
2012-03-31 16:00:40 +0900

    add array

--- Tiger/Tiger/src/Sum.java
+++ Tiger/Tiger/src/Sum.java
@@ -11,6 +11,10 @@ public class Sum {
 	
 	public static void main(String[] args) {
 		System.out.printf("%d %n",
-						  sum(1,2,3,4,5));
+						  sum(5,6,7,8,9));  // 可変長パラメータ
+
+		int[] arr = {1,2,3,4,5};
+		System.out.printf("%d %n",
+						  sum(arr)); // 配列パラメータ
 	}
 }
