b3e00decb3670453e176a5bbf7c2e6b322c33b1b
Ato ARAKI <ato.araki@gmail.com>
2012-04-05 19:48:05 +0900

    read P.66

---
+++ practice/Ch.03 Extending Classes/src/Attr.java
@@ -0,0 +1,36 @@
+/**
+ * P66. 名前と値の組を保持するクラス
+ * @author ato
+ *
+ */
+public class Attr {
+	private final String name;
+	private Object value = null;
+	
+	public Attr(String name) {
+		this.name = name;
+	}
+	
+	public Attr(String name, Object value) {
+		this.name = name;
+		this.value = value;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public Object getValue() {
+		return value;
+	}
+	
+	public Object setValue(Object newValue) {
+		Object oldVal = value;
+		value = newValue;
+		return oldVal;
+	}
+	
+	public String toString() {
+		return name + "='" + value + "'";
+	}
+}
