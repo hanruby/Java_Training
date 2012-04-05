5c8a0848e8aacf913131570857fd505e023d016a
Ato ARAKI <ato.araki@gmail.com>
2012-04-05 21:09:49 +0900

    Exercise 3.1
    Starting with the Vehicle class from the exercises in Chapter 2, create
    an extended class called PassengerVehicle to add a capability for
    counting the number of seats available in the car and the number
    currently occupied. Provide a new main method in PassengerVehicle to
    create a few of these objects and print them out.

--- JPL/ch03/src/ex03_01/PassengerVehicle.java
+++ JPL/ch03/src/ex03_01/PassengerVehicle.java
@@ -1,5 +1,39 @@
 package ex03_01;
 
-public class PassengerVehicle {
+public class PassengerVehicle extends Vehicle{
+	private int seatNum;
+	private int occupiedNum;
 
+	public PassengerVehicle(String ownerName, int seatNum, int occupiesNum) {
+		super(ownerName);
+		this.seatNum = seatNum;
+		this.occupiedNum = occupiesNum;
+	}
+	
+	public int getSeatNum() {
+		return seatNum;
+	}
+	
+	public int getOccupiedNum() {
+		return occupiedNum;
+	}
+	
+	@Override
+	public String toString() {
+		return super.toString() + " seat:" + this.seatNum
+								+ " occupied:" + this.occupiedNum;
+	}
+	
+	public static void main(String[] args) {
+		PassengerVehicle familyCar = new PassengerVehicle("Hadson", 4, 3);
+		familyCar.setCurrentSpeed(100);
+		familyCar.setCurrentDirection(134);
+				
+		PassengerVehicle sportsCar = new PassengerVehicle("Jenkins", 2, 2);
+		sportsCar.setCurrentSpeed(340);
+		sportsCar.setCurrentDirection(135);
+		
+		System.out.println(familyCar);
+		System.out.println(sportsCar);
+	}
 }
