8cf3e24f8cf256e8db2b8acc79d0826f0a5f2c3b
Ato ARAKI <ato.araki@gmail.com>
2012-04-08 16:33:21 +0900

    read P.76 paragraph 5

--- practice/Ch.03 Extending Classes/src/SuperShow.java
+++ practice/Ch.03 Extending Classes/src/SuperShow.java
@@ -4,10 +4,14 @@
  *
  */
 class SuperShow {
-	public String str = "SuperStr";
+	private String str = "SuperStr";
 	
 	public void show() {
-		System.out.println("Super.show: " + str);
+		System.out.println("Super.show: " + getStr());
+	}
+	
+	public String getStr() {
+		return str;
 	}
 
 	/** P.76 paragraph 5 line 1 */
@@ -17,12 +21,16 @@ class SuperShow {
 }
 
 class ExtendShow extends SuperShow {
-	public String str = "ExtendStr";
+	private String str = "ExtendStr";
 	
 	public void show() {
-		System.out.println("Extend.show: " + str);
+		System.out.println("Extend.show: " + getStr());
 	}
 	
+	public String getStr() {
+		return str;
+	}
+
 	public static void main(String[] args) {
 		ExtendShow ext = new ExtendShow();
 		SuperShow sup = ext;
@@ -30,8 +38,8 @@ class ExtendShow extends SuperShow {
 		sup.show();
 		ext.show();
 
-		System.out.println("sup.str = " + sup.str);
-		System.out.println("ext.str = " + ext.str);
+		System.out.println("sup.str = " + sup.getStr());
+		System.out.println("ext.str = " + ext.getStr());
 		
 		sup.show(ext);
 	}
