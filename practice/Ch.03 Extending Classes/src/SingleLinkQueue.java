285ded73fae20479f15df3cff3ad6f1a1026c597
Ato ARAKI <ato.araki@gmail.com>
2012-04-10 18:54:38 +0900

    read P.81

---
+++ practice/Ch.03 Extending Classes/src/SingleLinkQueue.java
@@ -0,0 +1,38 @@
+
+public class SingleLinkQueue {
+    protected Cell head;
+    protected Cell tail;
+
+    public void add(Object item) {/* ... */}
+    public Object remove() {/* ... */ return null;}
+}
+
+class Cell {
+    private Cell next;
+    private Object element;
+
+    public Cell(Object element) {
+        this.element = element;
+    }
+
+    public Cell(Object element, Cell next) {
+        this.element = element;
+        this.next = next;
+    }
+
+    public Object getElement() {
+        return element;
+    }
+
+    public void setElement(Object element) {
+        this.element = element;
+    }
+
+    public Cell getNext() {
+        return next;
+    }
+
+    public void setNext(Cell next) {
+        this.next = next;
+    }
+}
