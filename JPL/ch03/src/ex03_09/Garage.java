81eea68b72a22a4f3677b56cc50d396807063f48
Ato ARAKI <ato.araki@gmail.com>
2012-04-14 20:41:20 +0900

    Exercise 3.9
    Write a Garage class.

---
+++ JPL/ch03/src/ex03_09/Garage.java
@@ -0,0 +1,36 @@
+package ex03_09;
+
+import ex03_08.Vehicle;
+
+public class Garage implements Cloneable{
+    Vehicle[] vehicles;
+    
+    public Garage(Vehicle[] vehicles) {
+        if (vehicles == null) {
+            throw new IllegalArgumentException("argument is null");
+        }
+        this.vehicles = vehicles;
+    }
+    
+    public Vehicle[] getVehicles() {
+        return this.vehicles;
+    }
+    
+    /**
+     * ex03.09 clone
+     * 深いコピー
+     * ただし、Vehicleの ID は別のものを与える
+     */
+    @Override
+    protected Garage clone() {
+        Vehicle[] vehicles = new Vehicle[this.vehicles.length];
+        
+        for (int i = 0; i < vehicles.length; i++) {
+            vehicles[i] = this.vehicles[0].clone();
+        }
+            
+        Garage g = new Garage(vehicles);
+
+        return g;
+    }
+}
---
+++ JPL/ch03/src/ex03_09/GarageTest.java
@@ -0,0 +1,63 @@
+package ex03_09;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import ex03_08.Vehicle;
+
+public class GarageTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void testGarage() {
+        Garage g = new Garage(new Vehicle[]{ new Vehicle("Father"), new Vehicle("me")});
+        
+        Vehicle[] vehicles = g.getVehicles();
+        assertEquals(vehicles[0].getOwnerName(), "Father");
+        assertEquals(vehicles[1].getOwnerName(), "me");
+    }
+
+    @SuppressWarnings("unused")
+    @Test
+    public void testGarageArgNull() {
+        try {
+            Garage g1 = new Garage((Vehicle[])null);
+            fail("should be throw exception");
+        } catch (IllegalArgumentException e) {}
+
+        try {
+            Garage g2 = new Garage(null);
+            fail("should be throw exception");
+        } catch (IllegalArgumentException e) {}
+    }
+
+    @Test
+    public void testGetVehicles() {
+        Vehicle[] vs = new Vehicle[]{ new Vehicle("Father"), new Vehicle("me") };
+        Garage g = new Garage(vs);
+        
+        Vehicle[] res = g.getVehicles();
+        assertEquals(res[0].getOwnerName(), vs[0].getOwnerName());
+        assertEquals(res[0].getId(), vs[0].getId());
+
+        assertEquals(res[1].getOwnerName(), vs[1].getOwnerName());
+        assertEquals(res[1].getId(), vs[1].getId());
+    }
+
+    @Test
+    public void testClone() {
+        Vehicle[] vs = new Vehicle[]{ new Vehicle("Father"), new Vehicle("me") };
+        Garage g1 = new Garage(vs);
+        Garage g2 = g1.clone();
+
+        assertEquals(g1.getVehicles()[0].getOwnerName(), g2.getVehicles()[0].getOwnerName());
+        assertEquals(g1.getVehicles()[0].getId() + 2, g2.getVehicles()[0].getId());
+
+    }
+
+}
