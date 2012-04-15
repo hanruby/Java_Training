ccf59d879d3218b262bb3f2b70dc8b6ad8bf12f8
Ato ARAKI <ato.araki@gmail.com>
2012-04-15 17:22:36 +0900

    Exercise 3.11
    could not found security hole...

---
+++ JPL/ch03/src/ex03_11/HackSortDouble.java
@@ -0,0 +1,54 @@
+package ex03_11;
+
+public class HackSortDouble extends SortDouble implements Cloneable{
+    
+    @Override
+    protected SortDouble clone(){
+        System.out.println("clone!!");
+        try {
+            return (SortDouble)super.clone();
+        } catch (CloneNotSupportedException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+    
+    @Override
+    protected void doSort() {
+        // cloneしても問題ない。。
+        SortDouble c = this.clone();
+        HackSortDouble n = (HackSortDouble)this.clone();
+        
+        // オーバーフローさせるしかない？？
+        // for (long j = 0; j < 0x7FFFFFFF; j++) {
+        //     compare(0,0); //...... さらに繰り返す必要がある。
+        // }
+        
+        for (int i = 0; i < getDataLength(); i++) {
+            for (int j = i + 1; j < getDataLength(); j++) {
+                if (c.compare(i, j) > 0) {
+                    n.swap(i, j);
+                }
+            }
+        }
+        
+        // 結論、セキュリティホールはない・・・
+        
+        
+    }
+
+            
+    public static void main(String[] args) {
+        double[] testData = {
+            0.3, 1.3e-2, 7.9, 3.17
+        };
+        
+        System.out.println(HackSortDouble.class.getName());
+        
+        SortDouble bsort = new HackSortDouble();
+        SortMetrics metrics = bsort.sort(testData);
+        System.out.println("Metrics: " + metrics);
+        for (int i = 0; i < testData.length; i++)
+            System.out.println("\t" + testData[i]);
+    }
+}
