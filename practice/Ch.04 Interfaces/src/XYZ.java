8a7f2ab78d8ef989c537c49d7e7e01f9c1298bc4
Ato ARAKI <ato.araki@gmail.com>
2012-04-19 20:50:53 +0900

    read P.107

---
+++ practice/Ch.04 Interfaces/src/XYZ.java
@@ -0,0 +1,39 @@
+
+interface X {
+    int val = 1;
+}
+
+interface Y extends X {
+    int val = 2;
+    int sum = val + X.val;
+}
+
+class Z implements Y {}
+
+class XYZ {
+    @SuppressWarnings("static-access")
+    public static void main(String[] args) {
+
+        // P.106
+        // X.valを使用してXのvalにアクセスできる 
+        System.out.println("X.val = " + X.val);
+        
+        // P.107 
+        // Zを通してX.valを参照する方法はない
+        System.out.println("Z.val = " + Z.val + ", Z.sum = " + Z.sum);
+    
+        // Zのインスタンスがあれば、X.val
+        Z z = new Z();
+        System.out.println("z.val=" + z.val +
+                           ", ((Y)z).val=" + ((Y)z).val +
+                           ", ((X)z).val=" + ((X)z).val);
+        
+        // これらは、拡張したクラスのstaticフィールドに適用される規則と同じ
+    }
+}
+
+interface C {
+    String val = "Interface C";
+}
+
+interface D extends X, C {}
