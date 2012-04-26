d0ddb3bf6d5ec13d2fcbd6263a7f8d5a7b6790a8
Ato ARAKI <ato.araki@gmail.com>
2012-04-26 19:51:47 +0900

    read P.118

--- code/JPL/src/practice/ch05_Nested_Classes_and_Interfaces/BankAccount.java
+++ code/JPL/src/practice/ch05_Nested_Classes_and_Interfaces/BankAccount.java
@@ -4,7 +4,31 @@ package practice.ch05_Nested_Classes_and_Interfaces;
 public class BankAccount {
     private long number;    // 口座番号
     private long balance;   // 現在の残高（セント）
+    private Action lastAct; // 最後に行われた処理
 
+    public class Action {
+        private String act;
+        private long amount;
+        Action(String act, long amount) {
+            this.act = act;
+            this.amount = amount;
+        }
+        public String toString() {
+            // identify our enclosing account
+            return number + ": " + act + " " + amount;
+        }
+    }
+
+    public void deposit(long amount) {
+        balance += amount;
+        lastAct = new Action("deposit", amount);
+    }
+
+    public void withdraw(long amount) {
+        balance -= amount;
+        lastAct = new Action("withdraw", amount);
+    }
+    
     public static class Permissions {
         public boolean canDeposit,
                        canWithdraw,
