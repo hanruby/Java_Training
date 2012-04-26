1e1c281a0c21fbb9063599edeed6a3ea1bd2fd82
Ato ARAKI <ato.araki@gmail.com>
2012-04-26 20:50:58 +0900

    add base source

---
+++ code/JPL/src/ch05/ex05_02/BankAccount.java
@@ -0,0 +1,49 @@
+package ch05.ex05_02;
+
+/**
+ * ex05_02 口座に対する最後の10個の処理を記録するBankAccount
+ * @author ato
+ *
+ */
+@SuppressWarnings("unused")
+public class BankAccount {
+    private long number;    // 口座番号
+    private long balance;   // 現在の残高（セント）
+    private Action lastAct; // 最後に行われた処理
+
+    public class Action {
+        private String act;
+        private long amount;
+        Action(String act, long amount) {
+            this.act = act;
+            this.amount = amount;
+        }
+        public String toString() {
+            // identify our enclosing account
+            return number + ": " + act + " " + amount;
+        }
+    }
+
+    public void deposit(long amount) {
+        balance += amount;
+        lastAct = this.new Action("deposit", amount);
+    }
+
+    public void withdraw(long amount) {
+        balance -= amount;
+        lastAct = this.new Action("withdraw", amount);
+    }
+    
+    public static class Permissions {
+        public boolean canDeposit,
+                       canWithdraw,
+                       canClose;
+    }
+
+    public void transfer(BankAccount other, long amount) {
+        other.withdraw(amount);
+        deposit(amount);
+        lastAct = this.new Action("transfer", amount);
+        other.lastAct = other.new Action("transfer", amount);
+    }
+}
