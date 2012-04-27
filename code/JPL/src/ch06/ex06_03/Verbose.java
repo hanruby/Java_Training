90de42881f1e8d31da7c86e1db178ed2814215ee
Ato ARAKI <ato.araki@gmail.com>
2012-04-27 20:50:41 +0900

    Exercise 6.3
    modified Verbose class using an enum

--- code/JPL/src/ch06/ex06_03/Verbose.java
+++ code/JPL/src/ch06/ex06_03/Verbose.java
@@ -1,11 +1,14 @@
 package ch06.ex06_03;
 
-interface Verbose {
-    int SILENT = 0;
-    int TERSE = 1;
-    int NORMAL = 2;
-    int VERBOSE = 3;
+public interface Verbose {
+    public enum MessageLevel {
+        SILENT, 
+        TERSE,
+        NORMAL, 
+        VERBOSE;
+    }
     
-    void setVorbosity(int level);
-    int getVerbosity();
+    void setVorbosity(MessageLevel level);
+    MessageLevel getVerbosity();
 }
+
