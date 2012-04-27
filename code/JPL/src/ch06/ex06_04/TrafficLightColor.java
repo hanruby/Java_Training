58a1fe22f075f87bcbb85c28df73c69fbb578f5a
Ato ARAKI <ato.araki@gmail.com>
2012-04-27 22:22:06 +0900

    Exercise 6.4
    add Color object to traffic light color enum

--- code/JPL/src/ch06/ex06_04/TrafficLightColor.java
+++ code/JPL/src/ch06/ex06_04/TrafficLightColor.java
@@ -1,7 +1,27 @@
 package ch06.ex06_04;
 
+import java.awt.Color;
+
+/**
+ * 信号機の色
+ * @author ato
+ *
+ */
 public enum TrafficLightColor {
-    GREEN,
-    YELLOW,
-    RED;
+    GREEN(Color.GREEN),
+    YELLOW(Color.YELLOW),
+    RED(Color.RED);
+    
+    Color color;
+    TrafficLightColor(Color color) {
+        this.color = color;
+    }
+
+    public Color getColor() {
+        return this.color;
+    }
+    
+    public String toString() {
+        return color.toString();
+    }
 }
