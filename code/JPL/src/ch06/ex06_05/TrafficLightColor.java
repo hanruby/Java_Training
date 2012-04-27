dd50d82bef9a0578fb6028352c280109a434e43d
Ato ARAKI <ato.araki@gmail.com>
2012-04-27 23:34:05 +0900

    Exercise 6.5
    define constant-specific methods

--- code/JPL/src/ch06/ex06_05/TrafficLightColor.java
+++ code/JPL/src/ch06/ex06_05/TrafficLightColor.java
@@ -5,23 +5,30 @@ import java.awt.Color;
 /**
  * 信号機の色
  * @author ato
- *
+ * ex06_05 Colorオブジェクトを返すために、定数固有のメソッドを使用することは推奨しない。
+ * 理由：
+ *  - 定数固有の振る舞いとするべきような振る舞いがないため。
  */
 public enum TrafficLightColor {
-    GREEN(Color.GREEN),
-    YELLOW(Color.YELLOW),
-    RED(Color.RED);
+    GREEN {
+        @Override
+        Color getColor() {
+            return Color.GREEN;
+        }
+    },
+    YELLOW {
+        @Override
+        Color getColor() {
+            return Color.YELLOW;
+        }
+    },
+    RED {
+        @Override
+        Color getColor() {
+            return Color.RED;
+        }
+    };
     
-    Color color;
-    TrafficLightColor(Color color) {
-        this.color = color;
-    }
-
-    public Color getColor() {
-        return this.color;
-    }
-    
-    public String toString() {
-        return color.toString();
-    }
+    abstract Color getColor();
 }
+
