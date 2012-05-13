d4059cd634658a92f493490159ec71546600cba4
Ato ARAKI <ato.araki@gmail.com>
2012-05-13 23:42:34 +0900

    add other methods

--- code/JPL/src/ch09/ex09_02/BitCounter.java
+++ code/JPL/src/ch09/ex09_02/BitCounter.java
@@ -25,4 +25,32 @@ public class BitCounter {
         return count;
     }
     
+    /**
+     *  
+     * @param val
+     * @return
+     */
+    public static int bitCount2(int val) {
+        int count = 0;
+        for(; val!=0; val&=val-1 ){  
+            count++;
+        }
+        return count;
+    }
+
+    public static int bitCount3(int val) {
+        int num;
+        num = (val >> 1) & 03333333333;
+        num = val - num - ((num >> 1) & 03333333333);  
+        num = ((num + (num >> 3)) & 0707070707) % 077;
+        return num;
+    }
+    
+    public static int bitCount4(int val) {
+        val = (val & 0x55555555) + (val >> 1 & 0x55555555);
+        val = (val & 0x33333333) + (val >> 2 & 0x33333333);
+        val = (val & 0x0f0f0f0f) + (val >> 4 & 0x0f0f0f0f);
+        val = (val & 0x00ff00ff) + (val >> 8 & 0x00ff00ff);
+        return (val & 0x0000ffff) + (val >>16 & 0x0000ffff);
+    }
 }
--- code/JPL/src/ch09/ex09_02/BitCounterTest.java
+++ code/JPL/src/ch09/ex09_02/BitCounterTest.java
@@ -7,18 +7,46 @@ import org.junit.Test;
 
 public class BitCounterTest {
 
+    int[][] testData = {
+                        {0xAA, 4},
+                        {0xFF, 8},
+                        {0x01, 1}, 
+                        {0xFEFF, 15}, 
+                        {0x3FFFFFFF, 30}, 
+                        //{0xFFFFFFFF, 32}, 
+                        {1, 1}, 
+                       };
+    
     @Before
     public void setUp() throws Exception {
     }
-
+    
     @Test
     public void testBitCount() {
-        assertEquals(4, BitCounter.bitCount(0xAA));
-        assertEquals(8, BitCounter.bitCount(0xFF));
-        assertEquals(1, BitCounter.bitCount(0x01));
-        assertEquals(15, BitCounter.bitCount(0xFEFF));
-        assertEquals(32, BitCounter.bitCount(-1));
-        assertEquals(1, BitCounter.bitCount(1));
+        for (int index = 0; index < testData.length; index++) {
+            assertEquals(testData[index][1], BitCounter.bitCount(testData[index][0]));
+        }
+    }
+
+    @Test
+    public void testBitCount2() {
+        for (int index = 0; index < testData.length; index++) {
+            assertEquals(testData[index][1], BitCounter.bitCount2(testData[index][0]));
+        }
+    }
+
+    @Test
+    public void testBitCount3() {
+        for (int index = 0; index < testData.length; index++) {
+            assertEquals(testData[index][1], BitCounter.bitCount3(testData[index][0]));
+        }
+    }
+
+    @Test
+    public void testBitCount4() {
+        for (int index = 0; index < testData.length; index++) {
+            assertEquals(testData[index][1], BitCounter.bitCount4(testData[index][0]));
+        }
     }
 
 }
