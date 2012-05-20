3686015e3550e9d12a77e24084f3a99968ed477e
Ato ARAKI <ato.araki@gmail.com>
2012-05-20 13:59:14 +0900

    Exercise 10.1

---
+++ code/JPL/src/ch10/ex10_01/EscapeSpecialCharacters.java
@@ -0,0 +1,70 @@
+package ch10.ex10_01;
+
+/* P.201 */
+
+
+/* special characters 
+\n
+\t
+\b
+\r
+\f
+\\
+\'
+\"
+\ddd
+*/
+
+public class EscapeSpecialCharacters {
+    
+    /**
+     * 文字列のすべての特殊文字を対応する文字列に置き換えて返す
+     * @param str
+     * @return
+     */
+    public static String escape(String str) {
+        String escapedStr = "";
+        
+        for (int i = 0; i < str.length(); i++) {
+            escapedStr += escapeChar(str.charAt(i));
+        }
+        
+        return escapedStr;
+    }
+    
+    /**
+     * 特殊文字を文字列に置き換えて返す
+     * @param c
+     * @return
+     */
+    protected static String escapeChar(char c) {
+        if (c == '\n') {
+            return "\\n";
+        }
+        else if (c == '\t') {
+            return "\\t";
+        } 
+        else if (c == '\b') {
+            return "\\b";
+        } 
+        else if (c == '\r') {
+            return "\\r";
+        } 
+        else if (c == '\f') {
+            return "\\f";
+        } 
+        else if (c == '\\') {
+            return "\\\\";
+        } 
+        else if (c == '\'') {
+            return "\\'";
+        } 
+        else if (c == '\"') {
+            return "\\\"";
+        } 
+        else {
+            return String.valueOf(c);
+        }
+        
+    }
+}
