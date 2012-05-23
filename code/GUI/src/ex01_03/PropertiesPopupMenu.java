aad3869b082d60cef5a6d15e94f70314065b3afd
Ato ARAKI <ato.araki@gmail.com>
2012-05-23 22:33:05 +0900

    add changing font size function to popupmenu

--- code/GUI/src/ex01_03/PropertiesPopupMenu.java
+++ code/GUI/src/ex01_03/PropertiesPopupMenu.java
@@ -22,8 +22,8 @@ public class PropertiesPopupMenu extends PopupMenu{
             // Ref: http://www.java2s.com/Code/Java/Swing-JFC/AsimpleexampleofJPopupMenu.htm
 
             // Font name
-            Menu font_name_menu = new Menu("Font");
-            MenuItem font_item;
+            Menu menu = new Menu("Font");
+            MenuItem item;
  
             ActionListener menuListener = new ActionListener() {
                 public void actionPerformed(ActionEvent e) {
@@ -33,21 +33,48 @@ public class PropertiesPopupMenu extends PopupMenu{
                 }
             };
 
-            font_item = new MenuItem("Monaco");
-            font_item.addActionListener(menuListener);
-            font_name_menu.add(font_item);
+            item = new MenuItem("Monaco");
+            item.addActionListener(menuListener);
+            menu.add(item);
 
-            font_item = new MenuItem("Console");
-            font_item.addActionListener(menuListener);
-            font_name_menu.add(font_item);
+            item = new MenuItem("Console");
+            item.addActionListener(menuListener);
+            menu.add(item);
 
-            font_item = new MenuItem("Times");
-            font_item.addActionListener(menuListener);
-            font_name_menu.add(font_item);
+            item = new MenuItem("Times");
+            item.addActionListener(menuListener);
+            menu.add(item);
 
-            properties_menu.add(font_name_menu);
+            properties_menu.add(menu);
         }
-        
+        {
+            // Font size
+            Menu menu = new Menu("Font size");
+            MenuItem item;
+            
+            ActionListener menuListener = new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
+                    Config conf = new Config(clock.getConfig());
+                    conf.setFont(new Font(conf.getFont().getFontName(),conf.getFont().getStyle(),Integer.parseInt((String)e.getActionCommand())));
+                    clock.setConfig(conf);
+                }
+            };
+
+            item = new MenuItem("12"); 
+            item.addActionListener(menuListener);
+            menu.add(item);
+
+            item = new MenuItem("20"); 
+            item.addActionListener(menuListener);
+            menu.add(item);
+
+            item = new MenuItem("30");
+            item.addActionListener(menuListener);
+            menu.add(item);
+            
+            properties_menu.add(menu);
+        }
+
         add(properties_menu);
         
     }
