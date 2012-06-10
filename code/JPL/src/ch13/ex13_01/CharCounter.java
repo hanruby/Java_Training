e00a00bba7ead0b171e62028c3499c93d03bf31a
Ato ARAKI <ato.araki@gmail.com>
2012-06-10 22:38:50 +0900

    create countChar method

--- code/JPL/src/ch13/ex13_01/CharCounter.java
+++ code/JPL/src/ch13/ex13_01/CharCounter.java
@@ -2,7 +2,17 @@ package ch13.ex13_01;
 
 public class CharCounter {
     
-    static int countChar(String string, char c) {
-        return 0;
+    static int countChar(String str, char c) {
+        if( str == null ) {
+            throw new NullPointerException();
+        }
+        
+        int count = 0;
+        
+        for (int i = 0; i < str.length(); i++) {
+            if( str.charAt(i) == c ) count++;
+        }
+        
+        return count;
     }
 }
--- code/JPL/src/ch13/ex13_01/CharCounterTest.java
+++ code/JPL/src/ch13/ex13_01/CharCounterTest.java
@@ -15,10 +15,45 @@ public class CharCounterTest {
     }
 
     @Test
-    public void testCountChar() {
+    public void testCountChar_1() {
         
         result = CharCounter.countChar("aaaa",'a');
         assertEquals(4, result);
+        
     }
 
+    @Test
+    public void testCountChar_2() {
+        
+        result = CharCounter.countChar("abc",'a');
+        assertEquals(1, result);
+        
+    }
+
+    @Test
+    public void testCountChar_3() {
+
+        result = CharCounter.countChar("abcdefgabcdefgabcdefg",'a');
+        assertEquals(3, result);
+        
+    }
+
+    @Test
+    public void testCountChar_4() {
+
+        result = CharCounter.countChar("abcdefgabcdefgabcdefg",'g');
+        assertEquals(3, result);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testCountChar_Exception_1() {
+        
+        result = CharCounter.countChar("abc",(Character) null);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testCountChar_Exception_2() {
+        
+        result = CharCounter.countChar(null,'a');
+    }
 }
