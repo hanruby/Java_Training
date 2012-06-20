bebdcb29854fac0dc9535d831441f6df851189d0
Ato ARAKI <ato.araki@gmail.com>
2012-06-20 22:58:00 +0900

    rename value

--- Araki/JPL/ch13/ex13_06/NumberUtil.java
+++ Araki/JPL/ch13/ex13_06/NumberUtil.java
@@ -2,13 +2,13 @@ package ch13.ex13_06;
 
 public class NumberUtil {
     
-    private final static int PLACE = 3;
+    private final static int DIGITS = 3;
     
     static String numberFormatter(String numberString) {
         
         StringBuilder buf = new StringBuilder(numberString);
         
-        for (int place = buf.length() - PLACE; place > 0; place-= PLACE) {
+        for (int place = buf.length() - DIGITS; place > 0; place-= DIGITS) {
             buf.insert(place,",");
         }
         
