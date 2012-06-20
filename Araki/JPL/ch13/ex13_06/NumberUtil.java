67703e1c52f3d11ca7e542034334c70b96c6b7ef
Ato ARAKI <ato.araki@gmail.com>
2012-06-20 22:56:14 +0900

    Implement method

--- Araki/JPL/ch13/ex13_06/NumberUtil.java
+++ Araki/JPL/ch13/ex13_06/NumberUtil.java
@@ -14,4 +14,15 @@ public class NumberUtil {
         
         return buf.toString();
     }
+    
+    static String numberFormatter(String numberString, int digits, String separator) {
+        
+        StringBuilder buf = new StringBuilder(numberString);
+        
+        for (int place = buf.length() - digits; place > 0; place-= digits) {
+            buf.insert(place,separator);
+        }
+        
+        return buf.toString();
+    }
 }
