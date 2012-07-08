e12fc3f7a2d166d6c2cce724ac09e686a73640a3
Ato ARAKI <ato.araki@gmail.com>
2012-07-08 09:51:59 +0900

    add base source

---
+++ Araki/JPL/ch14/ex14_02/PrintQueue.java
@@ -0,0 +1,15 @@
+package ch14.ex14_02;
+
+public class PrintQueue {
+
+    public void add(PrintJob job) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    public PrintJob remove() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
