3a26b81e40b9f5da045e2bdc66803b6a8052076f
Ato ARAKI <ato.araki@gmail.com>
2012-07-08 18:44:13 +0900

    copy source file from ex14_04

---
+++ Araki/JPL/ch14/ex14_05/Adder.java
@@ -0,0 +1,18 @@
+package ch14.ex14_05;
+
+public class Adder {
+    private static int current_num = 0;
+    
+    public Adder() {
+        current_num = 0;
+    }
+    
+    public synchronized static void add(int num) {
+        current_num = current_num + num;
+        //System.out.println(this.current_num);
+    }
+    
+    public static int getNum() {
+        return current_num;
+    }
+}
