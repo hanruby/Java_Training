5676bf9b78562c73b121221eb5b5a5bcbc5d2998
Ato ARAKI <ato.araki@gmail.com>
2012-07-16 00:17:33 +0900

    implement function of show all members

--- Araki/JPL/ch16/ex16_03/ClassContents.java
+++ Araki/JPL/ch16/ex16_03/ClassContents.java
@@ -18,18 +18,54 @@ public class ClassContents {
             System.out.println("unknown class: " + args[0]);
         }
     }
+    
+    static void searchType(Type type) {
+        if (type == null) // 再帰呼び出し停止： スーパータイプが存在しない
+            return;
+        
+        Class<?> cls = null;
+        if (type instanceof Class<?>) {
+            cls = (Class<?>) type;
+        }
+        else if (type instanceof ParameterizedType) {
+            cls = (Class<?>) ((ParameterizedType)type).getRawType();
+        }
+        else {
+            throw new Error("Unexpected non-class type");
+        }
 
-    private static void printMembers(Member[] mems) {
+        System.out.println(cls);
+        
+        printMembers(cls.getFields());
+        printMembers(cls.getConstructors());
+        printMembers(cls.getMethods());
+
+                
+        searchType(cls.getGenericSuperclass());
+    }
+
+    // 標準出力に表示
+    private static java.io.PrintStream out = System.out;
+
+    public static void printMembers(Member[] mems) {
         for (Member m : mems) {
             if (m.getDeclaringClass() == Object.class)
                 continue;
             String decl = m.toString();
-            System.out.print("    ");
-            System.out.println(strip(decl, "java.lang."));
+            out.print("    ");
+            out.println(strip(decl, "java.lang."));
         }
     }
 
     protected static String strip(String base, String stripString) {
         return base.replace(stripString, "");
+    }
+    
+    public java.io.PrintStream getOut() {
+        return out;
+    }
+
+    public void setOut(java.io.PrintStream out) {
+        ClassContents.out = out;
     }
 }
--- Araki/JPL/ch16/ex16_03/ClassContentsTest.java
+++ Araki/JPL/ch16/ex16_03/ClassContentsTest.java
@@ -2,22 +2,61 @@ package ch16.ex16_03;
 
 import static org.junit.Assert.*;
 
+import java.io.BufferedOutputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 import org.junit.Before;
 import org.junit.Test;
 
 public class ClassContentsTest {
 
+    ClassContents cc = new ClassContents();
+    
+    private ByteArrayOutputStream _actual;
+    
+    private ByteArrayOutputStream _expected;
+    private PrintStream _out;
+
     @Before
     public void setUp() throws Exception {
+        _actual = new ByteArrayOutputStream();
+        _expected = new ByteArrayOutputStream();
+        
+        cc.setOut(new PrintStream(new BufferedOutputStream(_actual)));
+        _out = new PrintStream(new BufferedOutputStream(_expected));
     }
 
+    @Test
+    public void test1() throws Exception {
+        try {
+            Class<?> c = Class.forName("java.util.HashMap");
+            System.out.println(c);
+            ClassContents.printMembers(c.getFields());
+            ClassContents.printMembers(c.getConstructors());
+            ClassContents.printMembers(c.getMethods());
+        } catch (ClassNotFoundException e) {
+            System.out.println("unknown class : " + e);
+        }
+        
+        cc.getOut().flush();
+        //System.out.println(_actual.toString());
+    }
+    
+    @Test
+    public void testSearchType() throws Exception {
+        Class<?> startClass = Class.forName("java.util.HashMap");
+        
+        ClassContents.searchType(startClass);
+        
+        cc.getOut().flush();
+        System.out.println(_actual.toString());
+    }
+    
     @Test
     public void testStrip() {
         assertEquals("HashMap", ClassContents.strip("java.lang.HashMap", "java.lang."));
         assertEquals("  HashMap", ClassContents.strip("  java.lang.HashMap", "java.lang."));
         assertEquals("  ", ClassContents.strip("  java.lang.", "java.lang."));
     }
-    
-    
-
 }
