53c69efac89dd4d14f5a9571c8af0d83eb9b85fd
Ato ARAKI <ato.araki@gmail.com>
2012-07-16 21:08:01 +0900

    I read P.363

---
+++ Araki/JPL/practice/ch16_02_Annotation_Queries/AnnotationQueries.java
@@ -0,0 +1,27 @@
+package practice.ch16_02_Annotation_Queries;
+
+public class AnnotationQueries {
+
+    @BugsFixed({"45678", "43246"})
+    class Foo {}
+
+    /**
+     * P.342
+     */
+    @interface BugsFixed {
+        String[] value();
+    }
+
+    /**
+     * P.363
+     */
+    public static void main(String[] args) {
+
+        Class<Foo> cls = Foo.class;
+        BugsFixed bugsFixed = (BugsFixed) cls.getAnnotation(BugsFixed.class);
+        String[] bugsIDs = bugsFixed.value(); // This value of bugsIDs is null. Why?
+        for (String id : bugsIDs) {
+            System.out.println(id);
+        }
+    }
+}
