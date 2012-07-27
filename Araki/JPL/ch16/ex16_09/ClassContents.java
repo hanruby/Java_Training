b6f84282d1525b793161f2faafdea39d6adfc61d
Ato ARAKI <ato.araki@gmail.com>
2012-07-27 13:48:31 +0900

    Copy base source from ex16_05

---
+++ Araki/JPL/ch16/ex16_09/ClassContents.java
@@ -0,0 +1,99 @@
+package ch16.ex16_09;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.*;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * P.358 
+ *
+ */
+public class ClassContents {
+    
+    private static Set<String> members = new HashSet<String>();
+    
+    public static void main(String[] args) {
+        try {
+            Class<?> cls = Class.forName(args[0]);
+            out.println(cls);
+            searchType(cls);
+            showMembers();
+        } catch (ClassNotFoundException e) {
+            System.out.println("unknown class: " + args[0]);
+        }
+    }
+    
+    static void searchType(Type type) {
+        if (type == null) // 再帰呼び出し停止： スーパータイプが存在しない
+            return;
+        
+        Class<?> cls = null;
+        if (type instanceof Class<?>) {
+            cls = (Class<?>) type;
+        }
+        else if (type instanceof ParameterizedType) {
+            cls = (Class<?>) ((ParameterizedType)type).getRawType();
+        }
+        else {
+            throw new Error("Unexpected non-class type");
+        }
+
+        outputMembers(cls.getFields());
+        outputMembers(cls.getConstructors());
+        outputMembers(cls.getMethods());
+
+        searchType(cls.getGenericSuperclass());
+    }
+
+    public static void showMembers() {
+        for (String member : members) {
+            out.println("  "+ member);
+        }
+    }
+    
+    
+    // 標準出力に表示
+    private static java.io.PrintStream out = System.out;
+
+    public static void outputMembers(Member[] mems) {
+        for (Member m : mems) {
+            if (m.getDeclaringClass() == Object.class)
+                continue;
+            String decl = m.toString();
+            members.add(strip(decl, "java.lang.") + " " + outputAnnotation(m));
+        }
+    }
+    
+    public static String outputAnnotation(Member mem) {
+        Annotation[] annotations = new Annotation[0];
+
+        if (mem instanceof Field) {
+            annotations = ((Field)mem).getAnnotations();
+        }
+        if (mem instanceof Constructor) {
+            annotations = ((Constructor<?>)mem).getAnnotations();           
+        }
+        if (mem instanceof Method) {
+            annotations = ((Method)mem).getAnnotations();
+        }
+        
+        String annotationInfo = "";
+        for (Annotation annotation : annotations) {
+            annotationInfo += annotation.toString();
+        }
+        return annotationInfo;
+    }
+
+    protected static String strip(String base, String stripString) {
+        return base.replace(stripString, "");
+    }
+    
+    public java.io.PrintStream getOut() {
+        return out;
+    }
+
+    public void setOut(java.io.PrintStream out) {
+        ClassContents.out = out;
+    }
+}
---
+++ Araki/JPL/ch16/ex16_09/ClassContentsTest.java
@@ -0,0 +1,80 @@
+package ch16.ex16_09;
+
+import static org.junit.Assert.*;
+
+import java.io.BufferedOutputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class ClassContentsTest {
+
+    ClassContents cc = new ClassContents();
+    
+    private ByteArrayOutputStream _actual;
+    
+    private ByteArrayOutputStream _expected;
+    private PrintStream _out;
+
+    @Before
+    public void setUp() throws Exception {
+        _actual = new ByteArrayOutputStream();
+        _expected = new ByteArrayOutputStream();
+        
+        cc.setOut(new PrintStream(new BufferedOutputStream(_actual)));
+        _out = new PrintStream(new BufferedOutputStream(_expected));
+    }
+
+    @Test
+    public void testSearchType() throws Exception {
+
+        // Expected
+        _out.println("  public void ch16.ex16_05.A.methodA() @ch16.ex16_05.BugsFixed(value=[112233, 123456])");
+        _out.println("  public void ch16.ex16_05.X.methodX() @ch16.ex16_05.BugsFixed(value=[234567, 467211])");
+        _out.flush();
+        
+        Class<?> startClass = Class.forName("ch16.ex16_05.X");
+
+        ClassContents.searchType(startClass);
+        ClassContents.showMembers();
+        
+        cc.getOut().flush();
+        System.out.println(_actual.toString());
+        assertEquals(_expected.toString(), _actual.toString());
+    }
+    
+    @Test
+    public void testStrip() {
+        assertEquals("HashMap", ClassContents.strip("java.lang.HashMap", "java.lang."));
+        assertEquals("  HashMap", ClassContents.strip("  java.lang.HashMap", "java.lang."));
+        assertEquals("  ", ClassContents.strip("  java.lang.", "java.lang."));
+    }
+}
+
+
+class A {
+    public int num = 123;
+    @BugsFixed({"112233","123456"})
+    public void methodA(){};
+}
+
+class X extends A {
+    public int num;
+    @BugsFixed({"234567","467211"})
+    public void methodX(){};
+}
+
+
+/**
+ * P.342
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@interface BugsFixed {
+    String[] value();
+}
+
+
