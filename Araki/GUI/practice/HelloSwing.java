5f1e07224378308f4c0b91f06d21b260b57cd815
Ato ARAKI <ato.araki@gmail.com>
2012-07-27 10:51:10 +0900

    Add practice code of Swing

---
+++ Araki/GUI/practice/HelloSwing.java
@@ -0,0 +1,16 @@
+package practice;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+ 
+public class HelloSwing {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.getContentPane().add(new JLabel("Hello, world!"));
+        frame.pack(); 
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+}
+
