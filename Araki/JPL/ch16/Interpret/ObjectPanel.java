5d5a2e16687b1e76cf684684262b2e4dfaef02fe
Ato ARAKI <ato.araki@gmail.com>
2012-08-04 10:20:22 +0900

    Create Drag Drop function!

--- Araki/JPL/ch16/Interpret/ObjectPanel.java
+++ Araki/JPL/ch16/Interpret/ObjectPanel.java
@@ -1,46 +1,37 @@
 package ch16.Interpret;
 
 import java.awt.Dimension;
-import java.awt.datatransfer.DataFlavor;
-import java.awt.datatransfer.Transferable;
-import java.awt.datatransfer.UnsupportedFlavorException;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
-import java.io.IOException;
 import java.lang.reflect.*;
 
-import javax.activation.ActivationDataFlavor;
-import javax.activation.DataHandler;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
-import javax.swing.JComponent;
 import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 import javax.swing.JPopupMenu;
 import javax.swing.JScrollPane;
 import javax.swing.JTextField;
 import javax.swing.JTree;
-import javax.swing.TransferHandler;
 import javax.swing.tree.DefaultMutableTreeNode;
 import javax.swing.tree.DefaultTreeModel;
 
-public class OutlinePanel extends JPanel implements ActionListener{
+public class ObjectPanel extends JPanel implements ActionListener{
 
-    private static final long serialVersionUID = 671352749780328461L;
-    
+    private static final long serialVersionUID = 1L;
+
     private JTree tree;
     private DefaultMutableTreeNode classTree = new DefaultMutableTreeNode("Class");
     private DefaultTreeModel model;
     private JTextField text;
   
         
-    public OutlinePanel() {
+    public ObjectPanel() {
         
         tree = new JTree(classTree);
-        tree.setDragEnabled(true);
 
         JScrollPane treePanel = new JScrollPane(tree, 
                 JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, 
@@ -74,7 +65,6 @@ public class OutlinePanel extends JPanel implements ActionListener{
         MouseListener ml = new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
-
                 // Open the popupmenu using right click
                 if ( e.getButton() == MouseEvent.BUTTON3) {
 
@@ -91,7 +81,6 @@ public class OutlinePanel extends JPanel implements ActionListener{
                                 @Override
                                 public void actionPerformed(ActionEvent e) {
                                     System.out.println(obj);
-                                    Constructor<?> c = (Constructor<?>) obj;
                                 }
                             });
                             popup.add(exec);
@@ -111,41 +100,20 @@ public class OutlinePanel extends JPanel implements ActionListener{
             }
         };
         tree.addMouseListener(ml);
-        tree.setTransferHandler(new ObjectTransfer());
-    }
-    
-
-        
-    public void addMouseListener(MouseListener ml) {
-        this.tree.addMouseListener(ml);
     }
 
-    public void createClassTree(Class<?> cls) {
+    public void createObjectTree(Object obj, String name) {
 
-        DefaultMutableTreeNode root = new DefaultMutableTreeNode(cls.getCanonicalName());
+        DefaultMutableTreeNode root = new DefaultMutableTreeNode(name);
 
-        DefaultMutableTreeNode constructorTree = new DefaultMutableTreeNode("Constructor");
-        DefaultMutableTreeNode fieldTree = new DefaultMutableTreeNode("Field");
-        DefaultMutableTreeNode methodTree = new DefaultMutableTreeNode("Method");
-
-        Constructor<?>[] constructors = cls.getConstructors();
-        for (Constructor<?> constructor : constructors) {
-            constructorTree.add(new DefaultMutableTreeNode(constructor));
-        }
+        // Create object tree
         
-        Field[] fields = cls.getDeclaredFields();
-        for (Field field : fields) {
-            fieldTree.add(new DefaultMutableTreeNode(field));
-        }
+        // Create object method tree
+
+        // Create object field tree
         
-        Method[] methods = cls.getDeclaredMethods();
-        for (Method method : methods) {
-            methodTree.add(new DefaultMutableTreeNode(method));
-        }
-                
-        root.add(constructorTree);
-        root.add(fieldTree);
-        root.add(methodTree);
+        // Create object 
+        
         
         classTree.add(root);
         model.reload();
@@ -162,7 +130,7 @@ public class OutlinePanel extends JPanel implements ActionListener{
                 Class<?> cls = null;
                 try {
                     cls = Class.forName(name);
-                    createClassTree(cls);
+                    //createObjectTree(cls);
                 } catch (ClassNotFoundException e1) {
                     e1.printStackTrace();
                 }
@@ -191,83 +159,6 @@ public class OutlinePanel extends JPanel implements ActionListener{
     @Override
     public void setPreferredSize(Dimension arg0) {
         super.setPreferredSize(arg0);
-    }
-}
-
-class ObjectTransfer extends TransferHandler {
-
-    private static final long serialVersionUID = -1426178157203230501L;
-
-    public final static DataFlavor localObjectFlavor = new DataFlavor (
-            Constructor.class, "Constructor aa");
-
-    public static final DataFlavor[] flavors = {
-        localObjectFlavor
-    };
-
-    @Override
-    protected Transferable createTransferable(JComponent c) {
-        JTree tree = (JTree) c;
-        DefaultMutableTreeNode node = (DefaultMutableTreeNode) tree.getLastSelectedPathComponent();
-        if (node != null) {
-            Object obj = node.getUserObject();
-
-            // Accept constructor object.
-            if(obj instanceof Constructor<?>) {
-                DataHandler dh = new DataHandler(obj, localObjectFlavor.getMimeType());
-                return dh;
-            }
-        }
-        return null;
-    }
-
-    @Override
-    public int getSourceActions(JComponent arg0) {
-        return COPY_OR_MOVE;
-    }
-
-    @Override
-    public boolean canImport(TransferSupport support) {
-        // クリップボード経由のデータ転送は扱わない
-        if (!support.isDrop()) {
-            return false;
-        }
-
-        // ドロップする位置を常に表示する
-        support.setShowDropLocation(true);
-
-        // フレーバの指定
-        if (support.isDataFlavorSupported(localObjectFlavor)) {
-            return true;
-        }
-
-        return false;
-    }
-
-    @Override
-    public boolean importData(TransferSupport support) {
-        // クリップボード経由のデータ転送は扱わない
-        if (!support.isDrop()) {
-            return false;
-        }
-
-        JTextField text = (JTextField)support.getComponent();
-        try {
-            // ドロップデータ
-            Object obj = support.getTransferable().getTransferData(localObjectFlavor);
-            if (obj instanceof Constructor<?>) {
-                Constructor<?> constructor = (Constructor<?>) obj;
-                text.setText(constructor.toString());
-            }
-            return true;
-            
-        } catch (UnsupportedFlavorException ex) {
-            System.out.println(ex);
-        } catch (IOException ex) {
-            System.out.println(ex);
-        }
-
-        return false;
     }
 }
 
