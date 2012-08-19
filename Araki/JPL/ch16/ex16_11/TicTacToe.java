96e4aec49a764b6da685480565b889e30d6ce46a
Ato ARAKI <ato.araki@gmail.com>
2012-08-19 10:34:08 +0900

    Add judgment of winner

--- Araki/JPL/ch16/ex16_11/TicTacToe.java
+++ Araki/JPL/ch16/ex16_11/TicTacToe.java
@@ -7,10 +7,14 @@ public class TicTacToe {
         X ,
         O ,
     }
+
+    private Mark winner = Mark.None;
     
     private final int GRID_X = 3;
     private final int GRID_Y = 3;
     Mark[][] grid = new Mark[GRID_X][GRID_Y];
+
+    private int count = 0;
     
     public TicTacToe() {
         
@@ -28,13 +32,69 @@ public class TicTacToe {
         }
         if (grid[x][y] == Mark.None) {
             grid[x][y] = m;
+
+            count++;
             return true;
         }
         return false;
     }
     
-    public String getScore() {
+    public int getScore() {
+        return GRID_X*GRID_Y - count;
+    }
+    
+    public boolean isWin() {
 
-        return null;
+        // Horizontal
+        for (int y = 0; y < GRID_Y; y++) {
+            if ( isHorizontal(Mark.O, y) ) {
+                this.winner = Mark.O;
+                return true;
+            } 
+            else if ( isHorizontal(Mark.X, y) ) {
+                this.winner = Mark.X;
+                return true;
+            } 
+        }
+
+        // Vertical
+        for (int x = 0; x < GRID_X; x++) {
+            if ( isVertical(Mark.O, x) ) {
+                this.winner = Mark.O;
+                return true;
+            } 
+            else if ( isVertical(Mark.X, x) ) {
+                this.winner = Mark.X;
+                return true;
+            } 
+        }
+
+        // Diagonal
+        if ( isDiagonal(Mark.O) ) {
+            this.winner = Mark.O;
+            return true;
+        } 
+        else if ( isDiagonal(Mark.X) ) {
+            this.winner = Mark.X;
+            return true;
+        } 
+        
+        return false;
+    }
+    
+    private boolean isHorizontal (Mark m, int y) {
+        return ( grid[0][y] == m && grid[1][y] == m && grid[2][y] == m );
+    }
+
+    private boolean isVertical (Mark m, int x) {
+        return ( grid[x][0] == m && grid[x][1] == m && grid[x][2] == m );
+    }
+
+    private boolean isDiagonal (Mark m) {
+        return ( ( grid[0][0] == m && grid[1][1] == m && grid[2][2] == m ) || ( grid[0][2] == m && grid[1][1] == m && grid[2][0] == m ) ); 
+    }
+    
+    public Mark getWinner() {
+        return winner;
     }
 }
--- Araki/JPL/ch16/ex16_11/TicTacToeTest.java
+++ Araki/JPL/ch16/ex16_11/TicTacToeTest.java
@@ -55,4 +55,126 @@ public class TicTacToeTest {
         // 二回目はだめ
         assertFalse(t.setMark(0, 1, Mark.X));
     }
+    
+    @Test
+    public void test_isWin_Horizontal_1() throws Exception {		
+        TicTacToe t = new TicTacToe();
+        //
+        t.setMark(0, 0, Mark.X);
+        assertFalse(t.isWin());
+        assertEquals(Mark.None, t.getWinner());
+        t.setMark(1, 0, Mark.X);
+        assertFalse(t.isWin());
+        assertEquals(Mark.None, t.getWinner());
+        t.setMark(2, 0, Mark.X);
+        assertTrue(t.isWin());
+        assertEquals(Mark.X, t.getWinner());
+    }
+    
+    @Test
+    public void test_isWin_Horizontal_2() throws Exception {		
+        TicTacToe t = new TicTacToe();
+        //
+        t.setMark(0, 1, Mark.O);
+        assertFalse(t.isWin());
+        assertEquals(Mark.None, t.getWinner());
+        t.setMark(1, 1, Mark.O);
+        assertFalse(t.isWin());
+        assertEquals(Mark.None, t.getWinner());
+        t.setMark(2, 1, Mark.O);
+        assertTrue(t.isWin());
+        assertEquals(Mark.O, t.getWinner());
+    }
+    
+    @Test
+    public void test_isWin_Horizontal_3() throws Exception {		
+        TicTacToe t = new TicTacToe();
+        //
+        t.setMark(0, 2, Mark.X);
+        assertFalse(t.isWin());
+        assertEquals(Mark.None, t.getWinner());
+        t.setMark(1, 2, Mark.X);
+        assertFalse(t.isWin());
+        assertEquals(Mark.None, t.getWinner());
+        t.setMark(2, 2, Mark.X);
+        assertTrue(t.isWin());
+        assertEquals(Mark.X, t.getWinner());
+    }
+    
+    @Test
+    public void test_isWin_Vertical_1() throws Exception {		
+        TicTacToe t = new TicTacToe();
+        //
+        t.setMark(0, 0, Mark.X);
+        assertFalse(t.isWin());
+        assertEquals(Mark.None, t.getWinner());
+        t.setMark(0, 1, Mark.X);
+        assertFalse(t.isWin());
+        assertEquals(Mark.None, t.getWinner());
+        t.setMark(0, 2, Mark.X);
+        assertTrue(t.isWin());
+        assertEquals(Mark.X, t.getWinner());
+    }
+
+    @Test
+    public void test_isWin_Vertical_2() throws Exception {		
+        TicTacToe t = new TicTacToe();
+        //
+        t.setMark(1, 0, Mark.O);
+        assertFalse(t.isWin());
+        assertEquals(Mark.None, t.getWinner());
+        t.setMark(1, 1, Mark.O);
+        assertFalse(t.isWin());
+        assertEquals(Mark.None, t.getWinner());
+        t.setMark(1, 2, Mark.O);
+        assertTrue(t.isWin());
+        assertEquals(Mark.O, t.getWinner());
+    }
+
+    @Test
+    public void test_isWin_Vertical_3() throws Exception {		
+        TicTacToe t = new TicTacToe();
+        //
+        t.setMark(2, 0, Mark.X);
+        assertFalse(t.isWin());
+        assertEquals(Mark.None, t.getWinner());
+        t.setMark(2, 1, Mark.X);
+        assertFalse(t.isWin());
+        assertEquals(Mark.None, t.getWinner());
+        t.setMark(2, 2, Mark.X);
+        assertTrue(t.isWin());
+        assertEquals(Mark.X, t.getWinner());
+    }
+
+    @Test
+    public void test_isWin_Diagonal_1() throws Exception {		
+        TicTacToe t = new TicTacToe();
+        //
+        t.setMark(0, 0, Mark.X);
+        assertFalse(t.isWin());
+        assertEquals(Mark.None, t.getWinner());
+        t.setMark(1, 1, Mark.X);
+        assertFalse(t.isWin());
+        assertEquals(Mark.None, t.getWinner());
+        t.setMark(2, 2, Mark.X);
+        assertTrue(t.isWin());
+        assertEquals(Mark.X, t.getWinner());
+    }
+
+    @Test
+    public void test_isWin_Diagonal_2() throws Exception {		
+        TicTacToe t = new TicTacToe();
+        //
+        t.setMark(0, 2, Mark.X);
+        assertFalse(t.isWin());
+        assertEquals(Mark.None, t.getWinner());
+        t.setMark(1, 1, Mark.X);
+        assertFalse(t.isWin());
+        assertEquals(Mark.None, t.getWinner());
+        t.setMark(2, 0, Mark.X);
+        assertTrue(t.isWin());
+        assertEquals(Mark.X, t.getWinner());
+    }
+    
+    
 }
