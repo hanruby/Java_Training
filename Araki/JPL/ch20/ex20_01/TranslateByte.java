25531468358e39ebc50ac90443568fd4eae73b84
Ato ARAKI <ato.araki@gmail.com>
2012-08-24 20:45:46 +0900

    Rewrite main method to translate method

--- Araki/JPL/ch20/ex20_01/TranslateByte.java
+++ Araki/JPL/ch20/ex20_01/TranslateByte.java
@@ -5,13 +5,19 @@ import java.io.*;
 /**
  * P.443 <br> 
  * 入力を出力へコピーし、その過程で特定のバイト値を別の値へ変換する
+ * 
+ * 
  */
 public class TranslateByte {
 
     public static void main(String[] args) throws IOException {
         
-        byte from = (byte) args[0].charAt(0);
-        byte to = (byte) args[1].charAt(0);
+        translate(args[0],args[1]);
+    }
+
+    private static void translate(String in, String out) throws IOException {
+        byte from = (byte) in.charAt(0);
+        byte to = (byte) out.charAt(0);
         int b;
         
         while ((b = System.in.read()) != -1) {
