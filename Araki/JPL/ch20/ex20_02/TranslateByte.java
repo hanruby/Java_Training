0e88f568de666111e43f7e3b3f46c7837f0b0884
Ato ARAKI <ato.araki@gmail.com>
2012-08-24 23:13:45 +0900

    Copy source from ex20_01

---
+++ Araki/JPL/ch20/ex20_02/TranslateByte.java
@@ -0,0 +1,32 @@
+package ch20.ex20_02;
+
+import java.io.*;
+
+/**
+ * P.443 <br> 
+ * 入力を出力へコピーし、その過程で特定のバイト値を別の値へ変換する
+ */
+public class TranslateByte {
+
+    /**
+     * sample program <br>
+     * Usage : java path.to.classfile.TranslateByte b B <br>
+     * @param args
+     * @throws IOException
+     */
+    public static void main(String[] args) throws IOException {
+        
+        translate(System.in, System.out, (byte)args[0].charAt(0),(byte)args[1].charAt(0));
+    }
+
+    public static void translate(InputStream in, OutputStream out, byte from, byte to) throws IOException {
+
+        int b;
+        
+        while ((b = in.read()) != -1) {
+            out.write(b == from ? to : b);
+        }
+        
+        out.flush();
+    }
+}
---
+++ Araki/JPL/ch20/ex20_02/TranslateByteTest.java
@@ -0,0 +1,39 @@
+package ch20.ex20_02;
+
+
+import static org.junit.Assert.*;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class TranslateByteTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void testTranslate_01() throws Exception {
+        OutputStream out = new ByteArrayOutputStream();
+        InputStream in = new ByteArrayInputStream("abc".getBytes());
+        
+        TranslateByte.translate(in, out, (byte)'a',(byte)'A');
+
+        assertEquals("Abc", out.toString());
+    }
+    
+    @Test
+    public void testTranslate_02() throws Exception {
+        OutputStream out = new ByteArrayOutputStream();
+        InputStream in = new ByteArrayInputStream("abcabcabcabc".getBytes());
+        
+        TranslateByte.translate(in, out, (byte)'b',(byte)'B');
+
+        assertEquals("aBcaBcaBcaBc", out.toString());
+    }
+}
