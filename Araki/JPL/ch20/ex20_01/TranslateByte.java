3d69f38a395a06d6fc489461072ae607d35d2a1e
Ato ARAKI <ato.araki@gmail.com>
2012-08-24 23:04:16 +0900

    Add comment for main method

--- Araki/JPL/ch20/ex20_01/TranslateByte.java
+++ Araki/JPL/ch20/ex20_01/TranslateByte.java
@@ -5,11 +5,15 @@ import java.io.*;
 /**
  * P.443 <br> 
  * 入力を出力へコピーし、その過程で特定のバイト値を別の値へ変換する
- * 
- * 
  */
 public class TranslateByte {
 
+    /**
+     * sample program <br>
+     * Usage : java path.to.classfile.TranslateByte b B <br>
+     * @param args
+     * @throws IOException
+     */
     public static void main(String[] args) throws IOException {
         
         translate(System.in, System.out, (byte)args[0].charAt(0),(byte)args[1].charAt(0));
