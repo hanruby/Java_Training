e8fef06ef9163a674f46c256ced691ca710f633f
Ato ARAKI <ato.araki@gmail.com>
2012-08-27 15:43:37 +0900

    Implement readLine method

--- Araki/JPL/ch20/ex20_04/LineFilterReader.java
+++ Araki/JPL/ch20/ex20_04/LineFilterReader.java
@@ -1,6 +1,7 @@
 package ch20.ex20_04;
 
 import java.io.FilterReader;
+import java.io.IOException;
 import java.io.Reader;
 
 public class LineFilterReader extends FilterReader{
@@ -8,8 +9,37 @@ public class LineFilterReader extends FilterReader{
     protected LineFilterReader(Reader in) {
         super(in);
     }
+
+    private boolean carriageReturn = false;
     
-    public String readLine() {
-        return null;
+    public String readLine() throws IOException {
+        
+        StringBuilder builder = new StringBuilder();
+        int b;
+
+        while ((b = super.read()) != -1) {
+            
+            switch ((byte) b) {
+
+            case '\n':
+                if (carriageReturn == true) {
+                    carriageReturn = false;
+                    break;
+                }
+                carriageReturn = false;
+                return builder.toString();
+
+            case '\r':
+                carriageReturn = true;
+                return builder.toString();
+
+            default:
+                carriageReturn = false;
+                builder.append((char) b);
+                break;
+            }
+        }
+
+        return builder.toString();
     }
 }
--- Araki/JPL/ch20/ex20_04/LineFilterReaderTest.java
+++ Araki/JPL/ch20/ex20_04/LineFilterReaderTest.java
@@ -16,14 +16,16 @@ public class LineFilterReaderTest {
 
     @Test
     public void testReadLine() throws Exception {
-        StringReader in = new StringReader("aaa\nbbb\r\nccc\n");
+        StringReader in = new StringReader("aaa\nbbb\rccc\r\nddd\n\n");
         
         LineFilterReader lineFilterReader = new LineFilterReader(in); 
         
         assertEquals("aaa", lineFilterReader.readLine());
         assertEquals("bbb", lineFilterReader.readLine());
         assertEquals("ccc", lineFilterReader.readLine());
-        assertNull(lineFilterReader.readLine());
+        assertEquals("ddd", lineFilterReader.readLine());
+        assertEquals("", lineFilterReader.readLine());
+        
         
     } 
 }
