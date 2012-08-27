5aecf14e5609031f3f7b5d2cf643be08ff437469
Ato ARAKI <ato.araki@gmail.com>
2012-08-27 15:01:55 +0900

    Add test for Encrypt class

---
+++ Araki/JPL/ch20/ex20_03/EncryptOutputStreamTest.java
@@ -0,0 +1,30 @@
+package ch20.ex20_03;
+
+
+import static org.junit.Assert.*;
+
+import java.io.ByteArrayOutputStream;
+import java.io.OutputStream;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class EncryptOutputStreamTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void testEncrypt() throws Exception {
+        
+        byte[] test = {(byte) 0xAB, (byte) 0xBA};
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        EncryptOutputStream encrypt = new EncryptOutputStream(out);
+        
+        encrypt.write(test);
+        
+        assertArrayEquals(new byte[]{0x00, 0x11}, out.toByteArray());
+        
+    }
+}
