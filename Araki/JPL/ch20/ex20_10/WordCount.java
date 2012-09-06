428bdd3ab3e1fe6780278163b8074a7d32d9eb7e
Ato ARAKI <ato.araki@gmail.com>
2012-09-06 21:21:40 +0900

    Implement

---
+++ Araki/JPL/ch20/ex20_10/WordCount.java
@@ -0,0 +1,60 @@
+package ch20.ex20_10;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.io.StreamTokenizer;
+import java.util.HashMap;
+
+public class WordCount {
+    private HashMap<String, Integer> countTable;
+    private File file;
+
+    public WordCount(File file) {
+        this.countTable = new HashMap<String, Integer>();
+        this.file = file;
+    }
+
+    public void analyze() throws IOException {
+
+        Reader reader = new FileReader(file);
+        StreamTokenizer tokenizer = new StreamTokenizer(reader);
+
+        int tokenKind;
+
+        while ((tokenKind = tokenizer.nextToken()) != StreamTokenizer.TT_EOF) {
+
+            switch (tokenKind) {
+            case StreamTokenizer.TT_WORD:
+
+                String value = tokenizer.sval;
+                wordIncrement(value);
+                break;
+
+            case StreamTokenizer.TT_NUMBER:
+
+                String num = String.valueOf(tokenizer.nval);
+                wordIncrement(num);
+                break;
+
+            default:
+                break;
+            }
+        }
+    }
+
+    /**
+     * HashMapにあるキーの数をインクリメントする
+     * @param key
+     */
+    private void wordIncrement(String key) {
+
+        if (countTable.containsKey(key)) {
+            Integer count = countTable.get(key) + 1;
+            countTable.put(key, count);
+        } else {
+            countTable.put(key, 1);
+        }
+    }
+}
