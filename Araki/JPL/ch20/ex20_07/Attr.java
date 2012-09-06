7d544cf7e146099a7df7b30790d4c3e10245778d
Ato ARAKI <ato.araki@gmail.com>
2012-09-06 10:23:28 +0900

    Add Attr class

---
+++ Araki/JPL/ch20/ex20_07/Attr.java
@@ -0,0 +1,37 @@
+package ch20.ex20_07;
+
+/**
+ * Attr class from P.66 in JPL
+ */
+public class Attr {
+    private final String name;
+    private Object value = null;
+
+    public Attr(String name) {
+        this.name = name;
+    }
+
+    public Attr(String name, Object value) {
+        this.name = name;
+        this.value = value;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Object getValue() {
+        return value;
+    }
+
+    public Object setValue(Object newValue) {
+        Object oldVal = value;
+        value = newValue;
+        return oldVal;
+    }
+
+    public String toString() {
+        return name + "='" + value + "'";
+    }
+}
+
