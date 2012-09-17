74d7ee0c0becc1eb9fb463560b43d82f06e94279
Ato ARAKI <ato.araki@gmail.com>
2012-09-17 20:13:25 +0900

    Add DataHandler from P.400

---
+++ Araki/JPL/ch21/ex21_02/DataHandler.java
@@ -0,0 +1,33 @@
+package ch21.ex21_02;
+
+import java.io.File;
+import java.lang.ref.WeakReference;
+
+public class DataHandler {
+  
+    private File lastFile;    // 最後に読んだファイル
+    private WeakReference<byte[]> lastData;    // （おそらく）最後のデータ
+
+    byte[] readFile(File file) {
+        byte[] data;
+
+        // データを記憶しているか調べる
+        if (file.equals(lastFile)) {
+            data = lastData.get();
+            if (data != null)
+                return data;
+        }
+
+        // 記憶していないので、読み込む
+        data = readBytesFromFile(file);
+        lastFile = file;
+        lastData = new WeakReference<byte[]>(data);
+
+        return data;
+    }
+
+    private byte[] readBytesFromFile(File file) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+}
