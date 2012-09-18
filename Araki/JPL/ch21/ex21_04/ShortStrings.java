45608cc206f3b2699a50c03855cb7e0f5be367c8
Ato ARAKI <ato.araki@gmail.com>
2012-09-18 23:25:06 +0900

    Add comments from text

--- Araki/JPL/ch21/ex21_04/ShortStrings.java
+++ Araki/JPL/ch21/ex21_04/ShortStrings.java
@@ -3,37 +3,65 @@ package ch21.ex21_04;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
+/**
+ * 別のイテレータからStringオブジェクトを読み出して、
+ * 指定された長さより短い文字列オブジェクトだけを返すイテレータ型
+ * @see JPL P.536
+ */
 public class ShortStrings implements Iterator<String> {
     private Iterator<String> strings;  // 元の文字列
     private String nextShort;      // 次が不明ならnull
     private final int maxLen;      // この長さ以下の文字列を返す
 
-    public ShortStrings(Iterator<String> strings,
-                        int maxLen) {
+    /**
+     * 
+     * @param strings 文字列を提供するイテレータ 
+     * @param maxLen 最大長
+     */
+    public ShortStrings(Iterator<String> strings, int maxLen) {
         this.strings = strings;
         this.maxLen = maxLen;
         nextShort = null;
     }
 
+    /**
+     * @return 次の短い文字列を検索して、存在すればtrueを返す。
+     *         見つけることなく、文字列元のイテレータが終わると、falseを返す。
+     */
     public boolean hasNext() {
-        if (nextShort != null)  // すでに見つけた
+        // すでに見つけた
+        if (nextShort != null) {
             return true;
+        }
+        // 検索...
         while (strings.hasNext()) {
+            // nextShortに見つけた内容を記憶
             nextShort = strings.next();
-            if (nextShort.length() <= maxLen)
+            if (nextShort.length() <= maxLen) // 見つけた
                 return true;
         }
-        nextShort = null;       // 見つからなかった
+        // 見つからなかった
+        nextShort = null;
         return false;
     }
 
+    /**
+     * @return 次の短い文字列が存在するかを調べ、存在すればそれを返す
+     * @throws NoSuchElementException 何も返すものがない場合
+     */
     public String next() {
-        if (nextShort == null && !hasNext())
+        if (nextShort == null && !hasNext()) {
             throw new NoSuchElementException();
+        }
         String n = nextShort;   // nextShort を記憶する
         nextShort = null;       // nextShort を消す
         return n;               // nextShort を返す
     }
+
+    /**
+     * このイテレータ実装ではサポートしていないので、UnsupportedOperationExceptionを投げる
+     * @throws UnsupportedOperationException
+     */
     public void remove() {
         throw new UnsupportedOperationException();
     }
