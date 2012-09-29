7522622c464c643255ef58a7e5d691fdb8aa4c98
Ato ARAKI <ato.araki@gmail.com>
2012-09-29 15:52:32 +0900

    Add base source files of Attributed

---
+++ Araki/JPL/ch22/ex22_04/AttributedImpl.java
@@ -0,0 +1,32 @@
+package ch22.ex22_04;
+
+import java.util.Iterator;
+import java.util.Observable;
+
+public class AttributedImpl extends Observable implements Attributed {
+
+    @Override
+    public void add(Attr newAttr) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public Attr find(String attrName) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Attr remove(String attrName) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Iterator<Attr> attrs() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
