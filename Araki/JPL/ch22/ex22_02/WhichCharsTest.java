4c3155a8b9f4a0948bb35798be671076a7b5dab4
Ato ARAKI <ato.araki@gmail.com>
2012-09-29 09:59:36 +0900

    Add test code for WhichChars class

---
+++ Araki/JPL/ch22/ex22_02/WhichCharsTest.java
@@ -0,0 +1,25 @@
+package ch22.ex22_02;
+
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class WhichCharsTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+    
+    @Test
+    /**
+     * Verify that characters occurred is marked in a string.
+     */
+    public void test_charactersMarked() throws Exception {
+        WhichChars wc = new WhichChars("Testing 1 2 3");
+        
+        assertEquals("[ 123Teginst]", wc.toString());
+    }
+
+}
