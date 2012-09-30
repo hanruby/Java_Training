8bb139930c9424a8653c835297af9dd38e0e2e28
Ato ARAKI <ato.araki@gmail.com>
2012-09-30 15:30:07 +0900

    Copy source from ex22_07

---
+++ Araki/JPL/ch22/ex22_08/CSV.java
@@ -0,0 +1,49 @@
+package ch22.ex22_08;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.regex.MatchResult;
+import java.util.regex.Pattern;
+
+public class CSV {
+    static final int CELLS = 4;
+
+    public static List<String[]> readCSVTable(Readable source)
+            throws IOException {
+        return readCSVTable(source, CELLS);
+    }
+
+    public static List<String[]> readCSVTable(Readable source, int cells_num)
+            throws IOException {
+        Scanner in = new Scanner(source);
+        List<String[]> vals = new ArrayList<String[]>();
+        
+        // パターン生成
+        String exp = "^(.*),(.*),(.*),(.*)";
+        Pattern pat = Pattern.compile(exp, Pattern.MULTILINE);
+        
+        while (in.hasNextLine()) {
+            String line = in.findInLine(pat);
+            if (line != null) {
+                String[] cells = new String[cells_num];
+                MatchResult match = in.match();
+                for (int i = 0; i < cells_num; i++)
+                    cells[i] = match.group(i + 1);
+                vals.add(cells);
+                in.nextLine(); // 改行を読み飛ばす
+            }
+
+            else {
+                throw new IOException("input format error");
+            }
+        }
+
+        IOException ex = in.ioException();
+        if (ex != null)
+            throw ex;
+
+        return vals;
+    }
+}
---
+++ Araki/JPL/ch22/ex22_08/CSVTest.java
@@ -0,0 +1,69 @@
+package ch22.ex22_08;
+
+
+import static org.junit.Assert.*;
+
+import java.io.File;
+import java.io.FileReader;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class CSVTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void test_readCSVTable() throws Exception {
+        File file = new File("JPL/ch22/ex22_07/testdata.csv");
+        
+        List<String[]> list = CSV.readCSVTable(new FileReader(file));
+        
+        String[][] expected = {{"1","2","3","4",},{"4","3","2","1",}};
+        
+        int i = 0;
+        for (String[] strings : list) {
+            for (int col = 0; col < strings.length; col++) {
+                assertEquals(expected[i][col], strings[col]);
+            }
+            i++;
+        }
+    }
+
+    @Test
+    public void test_readCSVTable_3cell() throws Exception {
+        File file = new File("JPL/ch22/ex22_07/testdata.csv");
+        
+        List<String[]> list = CSV.readCSVTable(new FileReader(file), 3);
+        
+        String[][] expected = {{"1","2","3",},{"4","3","2",}};
+        
+        int i = 0;
+        for (String[] strings : list) {
+            for (int col = 0; col < strings.length; col++) {
+                assertEquals(expected[i][col], strings[col]);
+            }
+            i++;
+        }
+    }
+
+    @Test
+    public void test_readCSVTable_emptyline() throws Exception {
+        File file = new File("JPL/ch22/ex22_07/test_emptyline.csv");
+        
+        List<String[]> list = CSV.readCSVTable(new FileReader(file), 3);
+        
+        String[][] expected = {{"1","2","3",},{"4","3","2",}};
+        
+        int i = 0;
+        for (String[] strings : list) {
+            for (int col = 0; col < strings.length; col++) {
+                assertEquals(expected[i][col], strings[col]);
+            }
+            i++;
+        }
+    }
+}
