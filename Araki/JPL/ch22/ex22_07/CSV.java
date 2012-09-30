614221f880788165c828956088685baad4e4cf90
Ato ARAKI <ato.araki@gmail.com>
2012-09-30 15:49:06 +0900

    Rewrite pattern generate function and add tests

--- Araki/JPL/ch22/ex22_07/CSV.java
+++ Araki/JPL/ch22/ex22_07/CSV.java
@@ -19,8 +19,14 @@ public class CSV {
             throws IOException {
         Scanner in = new Scanner(source);
         List<String[]> vals = new ArrayList<String[]>();
-        String exp = "^(.*),(.*),(.*),(.*)";
-        Pattern pat = Pattern.compile(exp, Pattern.MULTILINE);
+        
+        // パターン生成
+        StringBuilder exp = new StringBuilder("^(.*)");
+        for (int cell = 1; cell < cells_num; cell++) {
+            exp.append(",(.*)");
+        }
+        
+        Pattern pat = Pattern.compile(exp.toString(), Pattern.MULTILINE);
         while (in.hasNextLine()) {
             String line = in.findInLine(pat);
             if (line != null) {
