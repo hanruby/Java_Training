aa01525b6984f3e85a7b21836addcc1decfd3817
Ato ARAKI <ato.araki@gmail.com>
2012-10-10 22:14:44 +0900

    Add test

---
+++ Araki/JPL/ch23/ex23_01/StreamPlugTest.java
@@ -0,0 +1,21 @@
+package ch23.ex23_01;
+
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class StreamPlugTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void testAddProgram() throws Exception {
+        Process proc = StreamPlug.userProg("ping -c 5 8.8.8.8");
+
+        assertEquals(proc.waitFor(), 0);
+    }
+}
