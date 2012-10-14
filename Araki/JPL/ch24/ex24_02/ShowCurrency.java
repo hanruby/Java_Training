9a18f42d2a64cdc011468bd394fc8d01e81d29b1
Ato ARAKI <ato.araki@gmail.com>
2012-10-14 15:56:06 +0900

    Implement currency symbol table

---
+++ Araki/JPL/ch24/ex24_02/ShowCurrency.java
@@ -0,0 +1,36 @@
+package ch24.ex24_02;
+
+import java.util.Currency;
+import java.util.Locale;
+
+public class ShowCurrency {
+    
+    static Locale[] locales = new Locale[] {
+        Locale.JAPAN,
+        Locale.KOREA,
+        Locale.CHINA,
+        Locale.FRANCE,
+        Locale.GERMANY,
+        Locale.US,
+        Locale.UK,
+        };
+    
+    static String[] ISO_4217 = new String[] {
+        "JPY", // ￥
+        "EUR", // €
+        "KRW", // ₩ ウォン
+        "NZD", // ニュージーランドドル
+        "USD", // アメリカドル
+        "AUD", // オーストラリアドル
+    };
+    
+    public static void main(String[] args) {
+        for (Locale locale : locales) {
+            System.out.printf(locale.toString());
+            for (String code : ISO_4217) {
+                System.out.printf("\t %s",Currency.getInstance(code).getSymbol(locale));
+            }
+            System.out.printf("%n");
+        }
+    }
+}
