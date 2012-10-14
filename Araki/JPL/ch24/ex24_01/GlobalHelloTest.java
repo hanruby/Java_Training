70b51301af36a1f3e631cb99ba874e884c3df0d3
Ato ARAKI <ato.araki@gmail.com>
2012-10-14 11:45:55 +0900

    Modified locale test

--- Araki/JPL/ch24/ex24_01/GlobalHelloTest.java
+++ Araki/JPL/ch24/ex24_01/GlobalHelloTest.java
@@ -4,6 +4,7 @@ package ch24.ex24_01;
 import static org.junit.Assert.*;
 
 import java.util.Locale;
+import java.util.ResourceBundle;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -16,15 +17,17 @@ public class GlobalHelloTest {
 
     @Test
     public void test_JAPAN() throws Exception {
-        
         Locale.setDefault(Locale.JAPAN);
-        GlobalHello.main(new String[]{});
+        
+        ResourceBundle res = ResourceBundle.getBundle("ch24.ex24_01.GlobalRes");
+        assertEquals("こんにちは", res.getString(GlobalRes.HELLO));
     }
 
     @Test
     public void test_ENGLISH() throws Exception {
-        
         Locale.setDefault(Locale.ENGLISH);
-        GlobalHello.main(new String[]{});
+        
+        ResourceBundle res = ResourceBundle.getBundle("ch24.ex24_01.GlobalRes");
+        assertEquals("hello", res.getString(GlobalRes.HELLO));
     }
 }
