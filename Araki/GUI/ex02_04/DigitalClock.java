b03e03600574c613349e492709df4c3d7a6faa0c
Ato ARAKI <ato.araki@gmail.com>
2012-10-19 00:17:31 +0900

    Add pupup menu

--- Araki/GUI/ex02_04/DigitalClock.java
+++ Araki/GUI/ex02_04/DigitalClock.java
@@ -3,10 +3,16 @@ package ex02_04;
 import java.awt.BorderLayout;
 import java.awt.Container;
 import java.awt.Dimension;
+import java.awt.Point;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
-import javax.swing.JFrame;
+import javax.swing.JMenuItem;
+import javax.swing.JPopupMenu;
 import javax.swing.JWindow;
 import javax.swing.SwingUtilities;
 import javax.swing.Timer;
@@ -19,7 +25,10 @@ public class DigitalClock extends JWindow implements ActionListener {
     private static final int DEFAULT_HEIGHT = 240;
 
     private Timer time;
-
+    private Config config;
+    
+    private JPopupMenu popupmenu;
+    
     public static void main(String[] args) {
         new DigitalClock();
     }
@@ -34,7 +43,8 @@ public class DigitalClock extends JWindow implements ActionListener {
         } catch (Exception e) {
             e.printStackTrace();
         }
-
+        config = new Config();
+        
         // set params
         this.setSize(DEFAULT_WIDTH, DEFAULT_HEIGHT);
 
@@ -52,16 +62,120 @@ public class DigitalClock extends JWindow implements ActionListener {
         clockPanel.setPreferredSize(new Dimension(DEFAULT_WIDTH,
                 (int) (DEFAULT_HEIGHT * 0.1)));
         contentPane.add(clockPanel, BorderLayout.SOUTH);
+        
+        // Event
+        addWindowListener(new ClockWindowListener());
+        MouseListener mouseEvent = new MouseListener();
+        addMouseListener(mouseEvent);
+        addMouseMotionListener(mouseEvent);
 
         time = new Timer(1000, (ActionListener) this);
         time.addActionListener(progressPanel);
         time.start();
+        
+        popupmenu = createMenuBar();
 
         this.setVisible(true);
     }
 
+    private JPopupMenu createMenuBar(){
+        JMenuItem propertiesMenu = new JMenuItem("Properties");
+        propertiesMenu.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent arg0) {
+                //configDialog.open();
+            }
+        });
+
+        JMenuItem quitMenu = new JMenuItem("Quit");
+        quitMenu.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                exitProcess();
+            }
+        });
+
+        JPopupMenu menuBar = new JPopupMenu();
+
+        menuBar.add(propertiesMenu);
+        menuBar.add(quitMenu);
+
+        return menuBar;
+    }
+            
     @Override
     public void actionPerformed(ActionEvent e) {
         repaint();
+    }
+    
+    class MouseListener extends MouseAdapter {
+        
+        Point dragStartPos, draggingPos, currentPos;
+        int mouseButton;
+        
+        @Override
+        public void mouseClicked(MouseEvent e) {
+
+            // 右クリックでポップアップメニューを開く
+            if ( e.getButton() == MouseEvent.BUTTON3) {
+                DigitalClock.this.popupmenu.show(e.getComponent(), e.getX(), e.getY());
+                
+            }
+            super.mouseClicked(e);
+        }
+        
+        @Override
+        public void mousePressed(MouseEvent e) {
+            
+            mouseButton = e.getButton();
+
+            // 左ドラッグ開始
+            if ( e.getButton() == MouseEvent.BUTTON1) {
+
+                // save drag start position.
+                dragStartPos = e.getPoint();
+            }
+            super.mousePressed(e);
+        }
+        
+        @Override
+        public void mouseDragged(MouseEvent e) {
+
+            // 左ドラッグ中
+            if ( e.getButton() == MouseEvent.BUTTON1 || mouseButton == MouseEvent.BUTTON1) {
+                draggingPos = e.getPoint();
+                currentPos = DigitalClock.this.getLocation();
+                Point newPos = new Point(currentPos.x + draggingPos.x - dragStartPos.x, currentPos.y + draggingPos.y - dragStartPos.y);
+                // set new window position
+                DigitalClock.this.setLocation(newPos);
+            }
+            
+            super.mouseDragged(e);
+        }
+    }
+    
+    /**
+     * 終了処理
+     */
+    public void exitProcess() {
+        this.config.setPosition(this.getLocation());
+        this.config.savePref();
+        System.out.println("exit.");
+        System.exit (0);
+    }
+
+    /**
+     * Windowがクローズされた場合
+     */
+    class ClockWindowListener extends WindowAdapter {
+        @Override
+        public void windowClosing (WindowEvent event) {
+            exitProcess();
+        }
+
+        @Override
+        public void windowClosed(WindowEvent arg0) {
+            exitProcess();
+        }
     }
 }
