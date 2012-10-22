12ca042c32fe968f5779a8e882672d049b1ac2b3
Ato ARAKI <ato.araki@gmail.com>
2012-10-22 22:29:21 +0900

    Remove warnings

--- Araki/GUI/ex02_04/DigitalClock.java
+++ Araki/GUI/ex02_04/DigitalClock.java
@@ -1,7 +1,6 @@
 package ex02_04;
 
 import java.awt.BorderLayout;
-import java.awt.Color;
 import java.awt.Container;
 import java.awt.Dimension;
 import java.awt.Point;
--- Araki/GUI/ex02_04/PropertiesDialog.java
+++ Araki/GUI/ex02_04/PropertiesDialog.java
@@ -12,7 +12,6 @@ import java.awt.GridBagLayout;
 import java.awt.GridLayout;
 import java.awt.Label;
 import java.awt.Panel;
-import java.awt.Rectangle;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
@@ -138,7 +137,7 @@ public class PropertiesDialog extends JDialog {
                 }
                 
                 font_color_list.select(config.getFontColor().toString()); // TODO fix
-                System.out.println(config.getFontColor().getClass().getName());
+                //System.out.println(config.getFontColor().getClass().getName());
                 font_color_list.addItemListener(new ItemListener() {
                     @Override
                     public void itemStateChanged(ItemEvent e) {
