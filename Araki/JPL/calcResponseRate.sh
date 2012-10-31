811aff341ac0ef286adce43547aad338d607dc2a
Ato ARAKI <ato.araki@gmail.com>
2012-10-31 20:15:07 +0900

    Modified calculation script

--- Araki/JPL/calcResponseRate.sh
+++ Araki/JPL/calcResponseRate.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 denominator=`ls -1 ch*/*/README.md | wc -l`
-numerator=`ls -1 ch*/*/*.java | sed 's$\(.*\)\(/.*\.java\)$\1$g' | uniq | wc -l`
+numerator=`ls -1 ch*/*/{*.java,answer.*} | sed 's$\(.*\)\(/.*\..*\)$\1$g' | uniq | wc -l`
 
 
 result=`expr $((numerator)) / $((denominator))`
@@ -13,6 +13,6 @@ echo "`echo "scale=2; $((numerator)).0 * 100 / $((denominator)).0 " | bc`%"
 
 TMP=/tmp/JPLlist.dat
 ls -1 ch*/*/README.md | sed 's$\(.*\)\(/.*\.md\)$\1$g' > $TMP
-ls -1 ch*/*/*.java | sed 's$\(.*\)\(/.*\.java\)$\1$g' | uniq | diff - $TMP | awk 'NF > 1 {print $2;}'
+ls -1 ch*/*/{*.java,answer.*} | sed 's$\(.*\)\(/.*\..*\)$\1$g' | uniq | diff - $TMP | awk 'NF > 1 {print $2;}'
 rm -f $TMP
 
