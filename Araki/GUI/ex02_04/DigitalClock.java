dca5293b8f7446dc85531bba52ca889e7ca706dc
Ato ARAKI <ato.araki@gmail.com>
2012-12-02 23:06:08 +0900

    Remove warnings

--- Araki/GUI/ex02_04/DigitalClock.java
+++ Araki/GUI/ex02_04/DigitalClock.java
@@ -7,8 +7,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ComponentEvent;
 import java.awt.event.ComponentListener;
-import java.awt.event.FocusEvent;
-import java.awt.event.FocusListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
