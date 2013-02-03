9f87f89427ec5721c30ede1e5209e7de5b91ddf0
Ato ARAKI <ato.araki@gmail.com>
2013-02-03 19:16:49 +0900

    Modified DataHandler class
    But I think need to some fix.

--- Araki/JPL/ch21/ex21_02/DataHandler.java
+++ Araki/JPL/ch21/ex21_02/DataHandler.java
@@ -5,12 +5,20 @@ import java.util.WeakHashMap;
 
 public class DataHandler {
   
-    private WeakHashMap<File, byte[]> weakMap = new WeakHashMap<File, byte[]>();
+    private File lastFile;
+    private WeakHashMap<File, byte[]> weakMap = new WeakHashMap<File, byte[]>(); // ホントはP400のようにWeakReferenceを使うべき。
 
     byte[] readFile(File file) {
         byte[] data;
 
         // データを記憶しているか調べる
+        if (file.equals(lastFile)) {
+            data = weakMap.get(file);
+            if (data != null) {
+                return data;
+            }
+            // weakMapはひとつだけの要素
+        }
         if (weakMap.containsKey(file)) {
             data = weakMap.get(file);
             if (data != null)
